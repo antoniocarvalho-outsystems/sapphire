80f4e58e21cd986a736f997dbbcdf3ec307c35ed
Antonio Carvalho <antonio.carvalho@outsystems.com>
2019-12-24 15:58:35 +0000

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const pkg = require('./package.json');
 const gulp = require('gulp');
 const {
     series
 } = require('gulp');
 const del = require('del');
-const exec = require('gulp-exec');
+const exec = require('child_process').exec;
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const bump = require('gulp-bump');
 const headerComment = require('gulp-header-comment');
+const log = require('fancy-log');
+var fs = require('fs');
 
 var paths = {
-    modules: 'src/modules.scss',
-    src: 'src/**/*.scss',
+    imports: 'src/imports.scss',
     dest: 'dist'
 };
 
@@ -25,13 +25,13 @@ function clean(cb) {
 
 function compileSass() {
     return (
-        gulp.src(paths.modules)
+        gulp.src(paths.imports)
         .pipe(sass())
         .pipe(rename({
             basename: 'styles'
         }))
         .pipe(headerComment(`
-        Version: ${pkg.version}
+        Version <%= pkg.version %>
         Generated on <%= moment().format() %>
         `))
         .pipe(gulp.dest(paths.dest))
@@ -40,7 +40,7 @@ function compileSass() {
 
 function bumpVersion() {
     return (
-        gulp.src('./package.json')
+        gulp.src(['./package.json'])
         .pipe(bump({
             type: 'patch',
             key: 'version'
@@ -49,14 +49,16 @@ function bumpVersion() {
     )
 }
 
-function sendTag() {
-    return (
-        gulp.src('./**/**')
-        .pipe(exec(`git tag ${pkg.version}`))
-        .pipe(exec(`git push ${pkg.version}`))
-    )
-}
+function createTag(cb) {
+    var pkg = JSON.parse(fs.readFileSync('./package.json'));
+    exec(`git tag ${pkg.version}`, cb);
+};
+
+// function createTag() {
+// var pkg = JSON.parse(fs.readFileSync('./package.json'));
+// return gulp.src('./**/**').pipe(exec(`git tag ${pkg.version}`))
+//}
+
 
 exports.default = series(clean, compileSass);
-exports.bumpVersion = bumpVersion;
-exports.sendTag = sendTag;
+exports.newtag = series(bumpVersion, compileSass, createTag);
