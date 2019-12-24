2b8e128ebbb3500190de3912cfb18bcb0f4838ce
Antonio Carvalho <antonio.carvalho@outsystems.com>
2019-12-24 13:21:01 +0000

    just testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const pkg = require('./package.json');
 const gulp = require('gulp');
 const {
     series
 } = require('gulp');
 const del = require('del');
+const exec = require('gulp-exec');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const bump = require('gulp-bump');
@@ -23,14 +25,13 @@ function clean(cb) {
 
 function compileSass() {
     return (
-        gulp
-        .src(paths.modules)
+        gulp.src(paths.modules)
         .pipe(sass())
         .pipe(rename({
             basename: 'styles'
         }))
         .pipe(headerComment(`
-        Version: <%= pkg.version %>
+        Version: ${pkg.version}
         Generated on <%= moment().format() %>
         `))
         .pipe(gulp.dest(paths.dest))
@@ -39,8 +40,7 @@ function compileSass() {
 
 function bumpVersion() {
     return (
-        gulp
-        .src('./package.json')
+        gulp.src('./package.json')
         .pipe(bump({
             type: 'patch',
             key: 'version'
@@ -49,5 +49,13 @@ function bumpVersion() {
     )
 }
 
+function sendTag() {
+    return (
+        gulp.src('./**/**')
+        .pipe(exec('git push --tags'))
+    )
+}
+
 exports.default = series(clean, compileSass);
 exports.bumpVersion = bumpVersion;
+exports.sendTag = sendTag;
