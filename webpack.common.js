ac23ab589860f1ac23db795f1ae172cb24f3efc1
Pablo Santos <pablo.santos@outsystems.com>
2020-01-29 11:53:31 +0000

    feat: webpack adjusments

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,12 @@
 const path = require('path');
-
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: path.resolve(__dirname, './src/components/index.js'),
 	output: {
-		filename: 'sapphire.scripts.js',
 		path: path.join(__dirname, 'dist'),
 		publicPath: '/dist/',
 	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: 'sapphire.styles.css',
-		}),
-	],
 	node: {
 		fs: 'empty',
 	},
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const common = require('./webpack.common.js');
 
