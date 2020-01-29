68bfcff048a8f17e2704f474dfdf70c1f307d1e4
Pablo Santos <pablo.santos@outsystems.com>
2020-01-29 13:15:23 +0000

    feat: lint

--- src/helpersHandleBar/links.js
+++ src/helpersHandleBar/links.js
@@ -1,25 +1,19 @@
-
-//import { LoremIpsum } from "lorem-ipsum";
-const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 /**
  * Registers the "foo" Handlebars helper.
  *
  * @param {object} handlebars The global Handlebars object used by kss-node's kssHandlebarsGenerator.
  */
 
+module.exports = handlebars => {
+	handlebars.registerHelper('btnTypes', value => {
+		const btnTypes = {
+			button: 'Primary',
+			second: 'Secondary',
+			third: 'Third',
+			link: 'Link',
+			cancel: 'Cancel',
+		};
 
-
-module.exports = function(handlebars) {
-  handlebars.registerHelper('btnTypes', function(value) {
-   const btnTypes = {
-     button:'Primary',
-     second:'Secondary',
-     third:'Third',
-     link:'Link',
-     cancel:'Cancel'
-   }
-
-
-    return new handlebars.SafeString(btnTypes[value]);
-  });
+		return new handlebars.SafeString(btnTypes[value]);
+	});
 };
--- src/helpersHandleBar/loremIpsum.js
+++ src/helpersHandleBar/loremIpsum.js
@@ -1,28 +1,24 @@
-
 //import { LoremIpsum } from "lorem-ipsum";
-const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const LoremIpsum = require('lorem-ipsum').LoremIpsum;
 /**
  * Registers the "foo" Handlebars helper.
  *
  * @param {object} handlebars The global Handlebars object used by kss-node's kssHandlebarsGenerator.
  */
 
-
-
 module.exports = function(handlebars) {
-  handlebars.registerHelper('lorem', function(arg1, arg2, options) {
-    const lorem = new LoremIpsum({
-      sentencesPerParagraph: {
-        max: 8,
-        min: 4
-      },
-      wordsPerSentence: {
-        max: 16,
-        min: 4
-      }
-    });
+	handlebars.registerHelper('lorem', function(arg1, arg2, options) {
+		const lorem = new LoremIpsum({
+			sentencesPerParagraph: {
+				max: 8,
+				min: 4,
+			},
+			wordsPerSentence: {
+				max: 16,
+				min: 4,
+			},
+		});
 
-
-    return new handlebars.SafeString(lorem.generateSentences(arg1));
-  });
+		return new handlebars.SafeString(lorem.generateSentences(arg1));
+	});
 };
