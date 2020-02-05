fb03d53497ada6bf903aae1335fe59a46cfbc2b7
Pablo Santos <pablo.santos@outsystems.com>
2020-02-05 10:06:28 +0000

    wip: header + layout base

--- webpack.common.js
+++ webpack.common.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const KssWebpackPlugin = require('kss-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const KssConfig = {
-	title: 'Sapphire StyleGuide',
-	css: '../dev.styles.css',
-	source: path.resolve(__dirname, './src/components'),
-	destination: path.resolve(__dirname, 'dist/styleguide'),
-	extend: path.resolve(__dirname, './src/helpersHandleBar'),
-};
-
-const outputPath = path.join(__dirname, 'dist');
-
-module.exports = {
-	entry: path.resolve(__dirname, './src/components/index.js'),
-	output: {
-		path: outputPath,
-		publicPath: '/dist/',
-	},
-	plugins: [
-		new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns: [`${outputPath}/*.hot-update.*`],
-    	dry: false,
-    	dangerouslyAllowCleanPatternsOutsideProject: true
-		}),
-		new KssWebpackPlugin(KssConfig)],
-	node: {
-		fs: 'empty',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.s?[ac]ss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'postcss-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: '[name].[ext]',
-					},
-				},
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: ['file-loader'],
-			},
-		],
-	},
-};
-=======
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const KssWebpackPlugin = require('kss-webpack-plugin');
@@ -137,4 +73,3 @@ module.exports = {
 		],
 	},
 };
->>>>>>> 02d169136102b96d6986d077caadcc56488520f5
