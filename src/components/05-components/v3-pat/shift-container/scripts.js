1939ace4de3c53cb12cbb382506f6be431559b25
Pablo Santos <pablo.santos@outsystems.com>
2020-02-28 16:56:13 +0000

    feat: shift container scripts

--- src/components/05-components/v3-pat/shift-container/scripts.js
+++ src/components/05-components/v3-pat/shift-container/scripts.js
@@ -1,110 +1,16 @@
 /* Component ShiftContainer */
-function ShiftContainer(shiftContainerId) {
-	$(function() {
+(function($, window, document, SapphireWidgets) {
+	var shiftTimelineResizeTimer;
+	let $shiftContainerId = $();
+
+	const create = shiftContainerId => {
 		$('.ShiftCard_timeLine').css('visibility', 'hidden');
 		$('.ShiftExpandable_container .ActionsMenu ').css('display', 'none');
-		var shiftTimelineResizeTimer;
 
-		var $shiftContainerId = $(shiftContainerId);
+		$shiftContainerId = $(shiftContainerId);
 
-		var hasScrollBar = function() {
-			var $ScrollableDiv = $shiftContainerId.find('.ShiftExpandable_container');
-			if ($shiftContainerId.find('.ShiftExpandable_container').length > 0) {
-				if ($ScrollableDiv.get(0).scrollHeight > $ScrollableDiv.height()) {
-					var $lastCell = $shiftContainerId.find('.IsTimer:last-child');
-					$lastCell.css('padding-right', '7px');
-				}
-			}
-		};
-
-		var redrawShiftTimeline = function() {
-			clearTimeout(shiftTimelineResizeTimer);
-			shiftTimelineResizeTimer = setTimeout(function() {
-				hasScrollBar();
-				handleShiftTable();
-			}, 400);
-		};
-
-		var handleShiftTable = function() {
-			$('.ShiftExpandable').each(function(index, el) {
-				var $elFlag = $(this)
-					.parent()
-					.find('.FlagLine');
-				var $elFlagTime = $(this)
-					.parent()
-					.find('.FlagLine_time');
-				var $columnFlag = $elFlag.data('column');
-				var $minutesFlag = $elFlag.data('minutes');
-				var $slots = $(this)
-					.closest('.ShiftContainer')
-					.find('.ShiftContainer_header')
-					.find('.ShiftCellContent');
-				var $slotWidth = Math.round($slots.eq(0).width());
-				var minutesPosition = ($minutesFlag * $slotWidth) / 60;
-
-				// handle current time flog horizontal positioning
-				var slotsPosition = [];
-				$slots.each(function(index, el) {
-					slotsPosition.push($(this).position().left);
-				});
-				$elFlag.css('left', slotsPosition[$columnFlag - 1] + minutesPosition);
-				$elFlag.css('display', 'block');
-				if ($columnFlag === $slots.length) {
-					$elFlagTime.css({
-						right: '1px',
-						left: 'auto',
-					});
-				}
-
-				// handle cells that might span over several slots
-				$(this)
-					.find('.ShiftCard')
-					.each(function(index, elRow) {
-						var rowHasSpannedCell = false;
-						$(elRow)
-							.find('.ShiftCellContent')
-							.each(function(index, elCell) {
-								var colspan = $(elCell).data('colspan');
-								if (colspan === slotsPosition.length || rowHasSpannedCell) {
-									$(elCell).css({
-										display: 'inline-block',
-										flex: '1 1 auto',
-									});
-								} else if (colspan > 1) {
-									rowHasSpannedCell = true;
-									$(elCell).css({
-										display: 'inline-block',
-										flex: 'none',
-										width: +(slotsPosition[colspan] - slotsPosition[0]) + 'px',
-									});
-								}
-							});
-					});
-
-				// handle horizontal scroll behavior
-				if (el.scrollWidth > el.clientWidth) {
-					$(el).width(el.scrollWidth);
-					$(this)
-						.closest('.ShiftContainer')
-						.find('.ShiftContainer_header')
-						.width(el.scrollWidth);
-				} else {
-					$(el).width('auto');
-					$(this)
-						.closest('.ShiftContainer')
-						.find('.ShiftContainer_header')
-						.width('auto');
-				}
-			});
-		};
 		hasScrollBar();
 		handleShiftTable();
-
-		SapphireWidgets.ShiftTimeline = {
-			redrawShiftTimeline: redrawShiftTimeline,
-			hasScrollBar: hasScrollBar,
-			handleShiftTable: handleShiftTable,
-		};
 
 		setTimeout(function() {
 			$('.ShiftCard_timeLine').css('visibility', 'visible');
@@ -115,45 +21,135 @@ function ShiftContainer(shiftContainerId) {
 			.off('mousedown')
 			.on('mousedown', function() {
 				setTimeout(function() {
-					SapphireWidgets.ShiftTimeline.redrawShiftTimeline();
+					redrawShiftTimeline();
 				}, 1500);
 			});
+	};
+
+	const handleShiftTable = () => {
+		$('.ShiftExpandable').each(function(index, el) {
+			var $elFlag = $(this)
+				.parent()
+				.find('.FlagLine');
+			var $elFlagTime = $(this)
+				.parent()
+				.find('.FlagLine_time');
+			var $columnFlag = $elFlag.data('column');
+			var $minutesFlag = $elFlag.data('minutes');
+			var $slots = $(this)
+				.closest('.ShiftContainer')
+				.find('.ShiftContainer_header')
+				.find('.ShiftCellContent');
+			var $slotWidth = Math.round($slots.eq(0).width());
+			var minutesPosition = ($minutesFlag * $slotWidth) / 60;
+
+			// handle current time flog horizontal positioning
+			var slotsPosition = [];
+			$slots.each(function(index, el) {
+				slotsPosition.push($(this).position().left);
+			});
+			$elFlag.css('left', slotsPosition[$columnFlag - 1] + minutesPosition);
+			$elFlag.css('display', 'block');
+			if ($columnFlag === $slots.length) {
+				$elFlagTime.css({
+					right: '1px',
+					left: 'auto',
+				});
+			}
+
+			// handle cells that might span over several slots
+			$(this)
+				.find('.ShiftCard')
+				.each(function(index, elRow) {
+					var rowHasSpannedCell = false;
+					$(elRow)
+						.find('.ShiftCellContent')
+						.each(function(index, elCell) {
+							var colspan = $(elCell).data('colspan');
+							if (colspan === slotsPosition.length || rowHasSpannedCell) {
+								$(elCell).css({
+									display: 'inline-block',
+									flex: '1 1 auto',
+								});
+							} else if (colspan > 1) {
+								rowHasSpannedCell = true;
+								$(elCell).css({
+									display: 'inline-block',
+									flex: 'none',
+									width: +(slotsPosition[colspan] - slotsPosition[0]) + 'px',
+								});
+							}
+						});
+				});
+
+			// handle horizontal scroll behavior
+			if (el.scrollWidth > el.clientWidth) {
+				$(el).width(el.scrollWidth);
+				$(this)
+					.closest('.ShiftContainer')
+					.find('.ShiftContainer_header')
+					.width(el.scrollWidth);
+			} else {
+				$(el).width('auto');
+				$(this)
+					.closest('.ShiftContainer')
+					.find('.ShiftContainer_header')
+					.width('auto');
+			}
+		});
+	};
+
+	const hasScrollBar = () => {
+		var $ScrollableDiv = $shiftContainerId.find('.ShiftExpandable_container');
+		if ($shiftContainerId.find('.ShiftExpandable_container').length > 0) {
+			if ($ScrollableDiv.get(0).scrollHeight > $ScrollableDiv.height()) {
+				var $lastCell = $shiftContainerId.find('.IsTimer:last-child');
+				$lastCell.css('padding-right', '7px');
+			}
+		}
+	};
+
+	const redrawShiftTimeline = () => {
+		clearTimeout(shiftTimelineResizeTimer);
+		shiftTimelineResizeTimer = setTimeout(function() {
+			hasScrollBar();
+			handleShiftTable();
+		}, 400);
+	};
+
+	const checkScroll = () => {
+		var hContent = $('.Content').height();
+		var hWindow = $(window).height();
+
+		if (hContent > hWindow) redrawShiftTimeline();
+	};
+
+	SapphireWidgets.ShiftContainer = { create, checkScroll, redrawShiftTimeline };
+})(jQuery, window, document, SapphireWidgets);
+
+$(window)
+	.off('resize.GenericGallery')
+	.on('resize.GenericGallery', function() {
+		$('.ShiftCard_timeLine').css('visibility', 'hidden');
+		$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
+
+		SapphireWidgets.ShiftContainer.redrawShiftTimeline();
+		osAjaxBackend.BindAfterAjaxRequest(SapphireWidgets.ShiftContainer.redrawShiftTimeline);
+		setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1000);
+
+		setTimeout(function() {
+			$('.ShiftCard_timeLine').css('visibility', 'visible');
+			$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
+		}, 1500);
 	});
-}
-
-function checkScrollBar() {
-	var hContent = $('.Content').height();
-	var hWindow = $(window).height();
-
-	if (hContent > hWindow) {
-		SapphireWidgets.ShiftTimeline.redrawShiftTimeline();
-	} else {
-	}
-}
-
-$(window).on('resize', function() {
-	$('.ShiftCard_timeLine').css('visibility', 'hidden');
-	$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
-
-	SapphireWidgets.ShiftTimeline.redrawShiftTimeline();
-	osAjaxBackend.BindAfterAjaxRequest(
-		SapphireWidgets.ShiftTimeline.redrawShiftTimeline
-	);
-	setTimeout(checkScrollBar, 1000);
-
-	setTimeout(function() {
-		$('.ShiftCard_timeLine').css('visibility', 'visible');
-		$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
-	}, 1500);
-});
 
 $(window).load(function() {
 	$('.ShiftCard_timeLine').css('visibility', 'hidden');
 	$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
 	setTimeout(function() {
-		SapphireWidgets.ShiftTimeline.redrawShiftTimeline();
+		SapphireWidgets.ShiftContainer.redrawShiftTimeline();
 	}, 1000);
-	setTimeout(checkScrollBar, 1000);
+	setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1000);
 	setTimeout(function() {
 		$('.ShiftCard_timeLine').css('visibility', 'visible');
 		$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
@@ -164,14 +160,14 @@ $(window).load(function() {
 			$('.ShiftCard_timeLine').css('visibility', 'hidden');
 			$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
 			setTimeout(function() {
-				SapphireWidgets.ShiftTimeline.redrawShiftTimeline();
+				SapphireWidgets.ShiftContainer.redrawShiftTimeline();
 			}, 1000);
-			setTimeout(checkScrollBar, 1500);
+			setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1500);
 			setTimeout(function() {
 				$('.ShiftCard_timeLine').css('visibility', 'visible');
 				$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
 			}, 1500);
-			SapphireWidgets.ShiftTimeline.redrawShiftTimeline;
+			SapphireWidgets.ShiftContainer.redrawShiftTimeline;
 		});
 	});
 
