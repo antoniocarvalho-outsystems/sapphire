94a08b02659fdc9ce7fda77643b9a2adde355b37
Antonio Carvalho <antonio.carvalho@outsystems.com>
2020-03-02 09:37:48 +0000

    styleguide

--- src/app.js
+++ src/app.js
@@ -27,3 +27,5 @@ require('./components/05-components/v3-pat/sidebar/sidebar-structure');
 require('./components/05-components/v3-pat/spinner-horizontal/scripts');
 require('./components/05-components/v3-pat/ssd-list-line/scripts');
 require('./components/05-components/v3-pat/tabs-extended/scripts');
+// ...
+require('./components/styleguide/scripts');
