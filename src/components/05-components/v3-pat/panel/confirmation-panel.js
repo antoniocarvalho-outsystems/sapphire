2c734dea1b70c1b89f416b7e026bba0bfac79488
Pablo Santos <pablo.santos@outsystems.com>
2020-03-04 11:12:24 +0000

    feat: colors adjustments

--- src/components/05-components/v3-pat/panel/confirmation-panel.js
+++ src/components/05-components/v3-pat/panel/confirmation-panel.js
@@ -1,19 +1,17 @@
 /* Component ConfirmationPanel3Options ConfirmationPanel same javascript code*/
 
-SapphireWidgets.ConfirmationPanel ={
-	isAnyPanelOpened:function () {
-		return (
-			$('body').hasClass('PanelOpened') && $('.PanelContainer:visible').length
-		);
+SapphireWidgets.ConfirmationPanel = {
+	isAnyPanelOpened: function() {
+		return $('body').hasClass('PanelOpened') && $('.PanelContainer:visible').length;
 	},
-	
-	togglePanel:function (PanelId) {
+
+	togglePanel: function(PanelId) {
 		if (!OsValidatorOnSubmit()) return;
-	
-		if (!isAnyPanelOpened()) {
+
+		if (!SapphireWidgets.ConfirmationPanel.isAnyPanelOpened()) {
 			$('body').addClass('PanelOpened');
 			$('#' + PanelId).fadeIn(140);
-	
+
 			setTimeout(function() {
 				$('#' + PanelId)
 					.find('.PanelContainer')
@@ -21,46 +19,34 @@ SapphireWidgets.ConfirmationPanel ={
 			}, 100);
 		}
 	},
-	
-	closePanel:function (PanelId) {
+
+	closePanel: function(PanelId) {
 		$('body').removeClass('PanelOpened');
 		$('#' + PanelId).fadeOut(140);
-	
+
 		setTimeout(function() {
 			$('#' + PanelId)
 				.find('.PanelContainer')
 				.slideUp(150);
 		}, 100);
 	},
-	
-	setPanelBehavior:function () {
+
+	setPanelBehavior: function() {
 		$('.Panel[panel-trigger-elementid]').each(function() {
 			var this_panel = $(this);
 			$('#' + this_panel.attr('panel-trigger-elementid') + '')
 				.off('click')
 				.on('click', function() {
-					togglePanel(this_panel.attr('id'));
+					SapphireWidgets.ConfirmationPanel.togglePanel(this_panel.attr('id'));
 					return false;
 				});
 		});
-	}		
-
-}
-
+	},
+};
 
 $(document).ready(function() {
 	SapphireWidgets.ConfirmationPanel.setPanelBehavior();
-	if (
-		osAjaxBackend.EventHandlers.AfterAjaxRequest.toString().indexOf(
-			'setPanelBehavior'
-		) == -1
-	) {
-		osAjaxBackend.BindAfterAjaxRequest(	SapphireWidgets.ConfirmationPanel.setPanelBehavior);
+	if (osAjaxBackend.EventHandlers.AfterAjaxRequest.toString().indexOf('setPanelBehavior') == -1) {
+		osAjaxBackend.BindAfterAjaxRequest(SapphireWidgets.ConfirmationPanel.setPanelBehavior);
 	}
 });
-
-	
-
-
-
-
