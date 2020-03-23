750d155cfb50ec045a7809f85bdbad72baf4fb12
Antonio Carvalho <antonio.carvalho@outsystems.com>
2020-03-23 15:06:42 +0000

    bug fixing

--- src/components/05-components/v3-pat/shift-container/scripts.js
+++ src/components/05-components/v3-pat/shift-container/scripts.js
@@ -1,10 +1,13 @@
 /* Component ShiftContainer */
-(function($, window, document, SapphireWidgets) {
-	var shiftTimelineResizeTimer;
+(function ($, window, document, SapphireWidgets) {
+
+	let shiftTimelineResizeTimer;
 	let $shiftContainerId = $();
 
 	const create = shiftContainerId => {
-		$('.ShiftCard_timeLine').css('visibility', 'hidden');
+
+		// $('.ShiftCard_timeLine').css('visibility', 'hidden');
+
 		$('.ShiftExpandable_container .ActionsMenu ').css('display', 'none');
 
 		$shiftContainerId = $(shiftContainerId);
@@ -12,40 +15,32 @@
 		hasScrollBar();
 		handleShiftTable();
 
-		setTimeout(function() {
-			$('.ShiftCard_timeLine').css('visibility', 'visible');
-			$('.ShiftExpandable_container .ActionsMenu ').css('display', 'block');
-		}, 1500);
+		// setTimeout(function () {
+		// 	$('.ShiftCard_timeLine').css('visibility', 'visible');
+		// 	$('.ShiftExpandable_container .ActionsMenu ').css('display', 'block');
+		// }, 1500);
 
-		$('.ShiftExpandable__arrow')
-			.off('mousedown')
-			.on('mousedown', function() {
-				setTimeout(function() {
-					redrawShiftTimeline();
-				}, 1500);
-			});
+		$('.ShiftExpandable__arrow').off('mousedown').on('mousedown', function () {
+			setTimeout(function () {
+				redrawShiftTimeline();
+			}, 1500);
+		});
+
 	};
 
 	const handleShiftTable = () => {
-		$('.ShiftExpandable').each(function(index, el) {
-			var $elFlag = $(this)
-				.parent()
-				.find('.FlagLine');
-			var $elFlagTime = $(this)
-				.parent()
-				.find('.FlagLine_time');
+		$('.ShiftExpandable').each(function (index, el) {
+			var $elFlag = $(this).parent().find('.FlagLine');
+			var $elFlagTime = $(this).parent().find('.FlagLine_time');
 			var $columnFlag = $elFlag.data('column');
 			var $minutesFlag = $elFlag.data('minutes');
-			var $slots = $(this)
-				.closest('.ShiftContainer')
-				.find('.ShiftContainer_header')
-				.find('.ShiftCellContent');
+			var $slots = $(this).closest('.ShiftContainer').find('.ShiftContainer_header').find('.ShiftCellContent');
 			var $slotWidth = Math.round($slots.eq(0).width());
 			var minutesPosition = ($minutesFlag * $slotWidth) / 60;
 
 			// handle current time flog horizontal positioning
 			var slotsPosition = [];
-			$slots.each(function(index, el) {
+			$slots.each(function (index, el) {
 				slotsPosition.push($(this).position().left);
 			});
 			$elFlag.css('left', slotsPosition[$columnFlag - 1] + minutesPosition);
@@ -58,43 +53,33 @@
 			}
 
 			// handle cells that might span over several slots
-			$(this)
-				.find('.ShiftCard')
-				.each(function(index, elRow) {
-					var rowHasSpannedCell = false;
-					$(elRow)
-						.find('.ShiftCellContent')
-						.each(function(index, elCell) {
-							var colspan = $(elCell).data('colspan');
-							if (colspan === slotsPosition.length || rowHasSpannedCell) {
-								$(elCell).css({
-									display: 'inline-block',
-									flex: '1 1 auto',
-								});
-							} else if (colspan > 1) {
-								rowHasSpannedCell = true;
-								$(elCell).css({
-									display: 'inline-block',
-									flex: 'none',
-									width: +(slotsPosition[colspan] - slotsPosition[0]) + 'px',
-								});
-							}
+			$(this).find('.ShiftCard').each(function (index, elRow) {
+				var rowHasSpannedCell = false;
+				$(elRow).find('.ShiftCellContent').each(function (index, elCell) {
+					var colspan = $(elCell).data('colspan');
+					if (colspan === slotsPosition.length || rowHasSpannedCell) {
+						$(elCell).css({
+							display: 'inline-block',
+							flex: '1 1 auto',
 						});
+					} else if (colspan > 1) {
+						rowHasSpannedCell = true;
+						$(elCell).css({
+							display: 'inline-block',
+							flex: 'none',
+							width: +(slotsPosition[colspan] - slotsPosition[0]) + 'px',
+						});
+					}
 				});
+			});
 
 			// handle horizontal scroll behavior
 			if (el.scrollWidth > el.clientWidth) {
 				$(el).width(el.scrollWidth);
-				$(this)
-					.closest('.ShiftContainer')
-					.find('.ShiftContainer_header')
-					.width(el.scrollWidth);
+				$(this).closest('.ShiftContainer').find('.ShiftContainer_header').width(el.scrollWidth);
 			} else {
 				$(el).width('auto');
-				$(this)
-					.closest('.ShiftContainer')
-					.find('.ShiftContainer_header')
-					.width('auto');
+				$(this).closest('.ShiftContainer').find('.ShiftContainer_header').width('auto');
 			}
 		});
 	};
@@ -111,7 +96,7 @@
 
 	const redrawShiftTimeline = () => {
 		clearTimeout(shiftTimelineResizeTimer);
-		shiftTimelineResizeTimer = setTimeout(function() {
+		shiftTimelineResizeTimer = setTimeout(function () {
 			hasScrollBar();
 			handleShiftTable();
 		}, 400);
@@ -124,57 +109,77 @@
 		if (hContent > hWindow) redrawShiftTimeline();
 	};
 
-	SapphireWidgets.ShiftContainer = { create, checkScroll, redrawShiftTimeline };
+	SapphireWidgets.ShiftContainer = {
+		create,
+		checkScroll,
+		redrawShiftTimeline
+	};
 })(jQuery, window, document, SapphireWidgets);
 
-$(window)
-	.off('resize.GenericGallery')
-	.on('resize.GenericGallery', function() {
+
+$(window).off('resize.GenericGallery').on('resize.GenericGallery', function () {
+
+	$('.ShiftCard_timeLine').css('visibility', 'hidden');
+	$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
+
+	SapphireWidgets.ShiftContainer.redrawShiftTimeline();
+
+	osAjaxBackend.BindAfterAjaxRequest(SapphireWidgets.ShiftContainer.redrawShiftTimeline);
+
+	setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1000);
+
+	setTimeout(function () {
+		$('.ShiftCard_timeLine').css('visibility', 'visible');
+		$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
+	}, 1500);
+
+});
+
+$(window).load(function () {
+	if (!!$('.ShiftContainer').length) {
+
 		$('.ShiftCard_timeLine').css('visibility', 'hidden');
 		$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
 
-		SapphireWidgets.ShiftContainer.redrawShiftTimeline();
-		osAjaxBackend.BindAfterAjaxRequest(SapphireWidgets.ShiftContainer.redrawShiftTimeline);
-		setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1000);
+		setTimeout(function () {
+			SapphireWidgets.ShiftContainer.redrawShiftTimeline();
+		}, 400);
 
-		setTimeout(function() {
+		setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 500);
+
+		setTimeout(function () {
 			$('.ShiftCard_timeLine').css('visibility', 'visible');
 			$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
-		}, 1500);
-	});
+		}, 600);
 
-$(window).load(function() {
-	$('.ShiftCard_timeLine').css('visibility', 'hidden');
-	$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
-	setTimeout(function() {
-		SapphireWidgets.ShiftContainer.redrawShiftTimeline();
-	}, 1000);
-	setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1000);
-	setTimeout(function() {
-		$('.ShiftCard_timeLine').css('visibility', 'visible');
-		$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
-	}, 15000);
-
-	osAjaxBackend.BindAfterAjaxRequest(function() {
-		$('.navigation-control-item').click(function() {
+		$('.navigation-control-item').off('click').on('click', function () {
 			$('.ShiftCard_timeLine').css('visibility', 'hidden');
 			$('.ShiftExpandable_container .ActionsMenu').css('display', 'none');
-			setTimeout(function() {
+		});
+
+		//Verify the scroll if tooltip opened
+		$('.ShiftExpandable_container').on('scroll', function () {
+			if ($('.tooltipster-base').is(':visible')) {
+				$('.tooltipster-base').css('visibility', 'hidden');
+			}
+		});
+
+		osAjaxBackend.BindAfterAjaxRequest(function () {
+
+			setTimeout(function () {
 				SapphireWidgets.ShiftContainer.redrawShiftTimeline();
-			}, 1000);
-			setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 1500);
-			setTimeout(function() {
+			}, 400);
+
+			setTimeout(SapphireWidgets.ShiftContainer.checkScroll, 500);
+
+			setTimeout(function () {
 				$('.ShiftCard_timeLine').css('visibility', 'visible');
 				$('.ShiftExpandable_container .ActionsMenu').css('display', 'block');
-			}, 1500);
-			SapphireWidgets.ShiftContainer.redrawShiftTimeline;
-		});
-	});
+			}, 600);
 
-	//Verify the scroll if tooltip opened
-	$('.ShiftExpandable_container').on('scroll', function() {
-		if ($('.tooltipster-base').is(':visible')) {
-			$('.tooltipster-base').css('visibility', 'hidden');
-		}
-	});
+			// SapphireWidgets.ShiftContainer.redrawShiftTimeline;
+
+		});
+
+	}
 });
--- src/xx_garbage/dummy2.js
+++ src/xx_garbage/dummy2.js
@@ -1,72 +1,101 @@
-var SapphireWidgets = (window.SapphireWidgets = window.SapphireWidgets || {});
+function DosageBlock(config) {
+  $(function () {
+    var dosageBlockId = config.DosageBlockId;
+    var $dosageBlock = $('#' + dosageBlockId);
+    var startCol = config.StartCol;
+    var endCol = config.EndCol;
+    var $allTimeSlots = $('.ShiftContainer').find('.ShiftContainer_header').find('.ShiftCellContent');
+    var numberTimeSlots = $allTimeSlots[1].getBoundingClientRect().width;
+    var colFill = parseInt(endCol) - parseInt(startCol) + 1;
+    var minuteValueWidth = numberTimeSlots / 60;
+    var i;
+    var totalLeft = 0;
 
-function ifFnExists(fnName) {
-  fn = window[fnName];
-  fnExists = typeof fn === 'function';
-  return fnExists;
-}
 
-$.fn.setCursorPosition = function (pos) {
-  this.each(function (index, elem) {
-    if (elem.setSelectionRange) {
-      elem.setSelectionRange(pos, pos);
-    } else if (elem.createTextRange) {
-      var range = elem.createTextRange();
-      range.collapse(true);
-      range.moveEnd('character', pos);
-      range.moveStart('character', pos);
-      range.select();
+    for (i = 1; i < parseInt(startCol); i++) {
+      totalLeft += $allTimeSlots[i].getBoundingClientRect().width;
     }
-  });
-  return this;
-};
 
-function getURLParameter(name) {
-  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
-}
-
-function changeUrlParam(param, value) {
-  var currentURL = window.location.href + '&';
-  var change = new RegExp('(' + param + ')=(.*)&', 'g');
-  var newURL = currentURL.replace(change, '$1=' + value + '&');
-
-  if (getURLParameter(param) !== null) {
-    try {
-      window.history.replaceState('', '', newURL.slice(0, -1));
-    } catch (e) {
-      console.log(e);
-    }
-  } else {
-    var currURL = window.location.href;
-    if (currURL.indexOf("?") !== -1) {
-      window.history.replaceState('', '', currentURL.slice(0, -1) + '&' + param + '=' + value);
+    $dosageBlock.width(colFill * numberTimeSlots);
+    if ($('.Page').hasClass('AR') || $('.Page').hasClass('FA')) {
+      $dosageBlock.css('right', totalLeft + 'px');
     } else {
-      window.history.replaceState('', '', currentURL.slice(0, -1) + '?' + param + '=' + value);
+      $dosageBlock.css('left', totalLeft + 'px');
     }
-  }
-}
 
-SapphireWidgets.ElementEventBlocker = {};
+    var checkForOverlap = function (el1, el2) {
+      var bounds1 = el1.getBoundingClientRect();
+      var bounds2 = el2.getBoundingClientRect();
+      var firstIstLeftmost = (bounds1.left <= bounds2.left);
+      var leftest = firstIstLeftmost ? bounds1 : bounds2;
+      var rightest = firstIstLeftmost ? bounds2 : bounds1;
 
-SapphireWidgets.ElementEventBlocker.block = function (buttonId) {
-  $('#' + buttonId).addClass('element-blocked');
-}
+      //change > to to >= if border overlap should count
+      if (leftest.right > rightest.left) {
+        var firstIsTopmost = (bounds1.top <= bounds2.top);
+        var topest = firstIsTopmost ? bounds1 : bounds2;
+        var bottomest = firstIsTopmost ? bounds2 : bounds1;
+        //change > to >= if border should count 
+        return topest.bottom > bottomest.top;
+      } else return false;
+    }
 
-function bindBlockableElement() {
-  $('.blockable-element').off('mousedown').on('mousedown', function () {
-    var buttonId = $(this).prop('id');
-    SapphireWidgets.ElementEventBlocker.block(buttonId);
-    $(this).off('mousedown');
-    $(this).trigger('click');
+
+    var $shiftCardLine = $dosageBlock.closest('.ShiftCard_timeLine');
+    var CardShiftCount = $shiftCardLine.find('.CardShiftWrapperContainer').length;
+
+    if (CardShiftCount > 0) {
+      var count = 0;
+      $shiftCardLine.find('.CardShiftWrapperContainer').each(function () {
+        var $this = $(this);
+        var $shiftCard = $this.closest('.ShiftCard_timeLine');
+        var overlaped = checkForOverlap($dosageBlock[0], $this[0]);
+        var createdId = $dosageBlock.attr('id');
+        var LoopedId = $this.attr('id');
+        if (overlaped === true && count < $shiftCardLine.find('.TakeSlot').length) {
+          $dosageBlock.css('top', $this[0].offsetTop + $dosageBlock.height());
+        }
+        count = count++;
+      });
+      $('.ShiftCard_timeLine .TakeSlot').each(function () {
+        var $this = $(this);
+        var $shiftCard = $this.closest('.ShiftCard_timeLine');
+        var overlaped = checkForOverlap($dosageBlock[0], $this[0]);
+        var createdId = $dosageBlock.attr('id');
+        var LoopedId = $this.attr('id');
+        var $firstSlotCreated = $('.ShiftCard_timeLine .TakeSlot')[0];
+        if (overlaped === true && createdId != LoopedId && createdId != $firstSlotCreated.id) {
+          $dosageBlock.css('top', $this[0].offsetTop + 130);
+        }
+      });
+      $shiftCardLine.height($shiftCardLine.find('.TakeSlot').last().position().top + 130);
+    } else {
+      $('.ShiftCard_timeLine .TakeSlot').each(function () {
+        var $this = $(this);
+        var $shiftCard = $this.closest('.ShiftCard_timeLine');
+        var overlaped = checkForOverlap($dosageBlock[0], $this[0]);
+        var createdId = $dosageBlock.attr('id');
+        var LoopedId = $this.attr('id');
+        var $firstSlotCreated = $('.ShiftCard_timeLine .TakeSlot')[0];
+        if (overlaped === true && createdId != LoopedId && createdId != $firstSlotCreated.id) {
+          $dosageBlock.css('top', $dosageBlock[0].offsetTop + 130 + 'px');
+          $shiftCard.height($shiftCard.height() + $this[0].offsetHeight);
+        }
+      });
+    }
+
+    $dosageBlock.find('.ProgressBarWrap').each(function (index) {
+      var beginslot = $(this).data('beginslot');
+      var beginminute = $(this).data('beginminute');
+      var endslot = $(this).data('endslot');
+      var endminute = $(this).data('endminute');
+
+      var progressBarMinutes = ((endslot - beginslot) * 60) + endminute - beginminute;
+      var progressendWithPer = progressBarMinutes * minuteValueWidth;
+
+      $(this).css('left', (((beginslot - 1) * 60) + beginminute) * minuteValueWidth + 'px');
+      $(this).css('width', progressendWithPer + 'px');
+    });
+
   });
 }
-
-$(function () {
-  bindBlockableElement();
-});
-
-$(window).load(function () {
-  osAjaxBackend.BindAfterAjaxRequest(function () {
-    bindBlockableElement();
-  });
-});
