bc54ca430219595b938d9a2319a6d568f0c3f166
Pablo Santos <pablo.santos@outsystems.com>
2020-03-25 18:32:18 +0000

    feat: spinner vertical

---
+++ src/components/05-components/v3-pat/spinner-vertical/scripts.js
@@ -0,0 +1,172 @@
+/* Component SpinnerVertical */
+(function($, window, document, SapphireWidgets) {
+	const create = function(config) {
+		$(document).ready(function() {
+			var MinValue = ' + MinValue + ';
+
+			$(`#${config.widgetID} .SpinnerInputVertical input`).on('blur keyup input', function() {
+				var CurrentInputValue = $(`#${config.widgetID} .SpinnerInputVertical input`).val();
+
+				if (CurrentInputValue < MinValue) {
+					$(`#${config.widgetID}`)
+						.find('.MinusVertical')
+						.addClass('DisabledSpin');
+				} else {
+					$(`#${config.widgetID}`)
+						.find('.MinusVertical')
+						.removeClass('DisabledSpin');
+				}
+			});
+
+			if ($(`#${config.widgetID} .SpinnerInputVertical input`).val() <= MinValue) {
+				$(`#${config.widgetID}`)
+					.find('.MinusVertical')
+					.addClass('DisabledSpin');
+			}
+
+			if (config.isCursorOnFocus) {
+				$('body').on('focus', `#${config.inputID} input`, function() {
+					var that = this;
+
+					setTimeout(function() {
+						that.selectionStart = that.selectionEnd = 100000;
+					}, 300);
+				});
+			}
+		});
+	};
+
+	const increment = (elementId, minValue, maxValue, triggerOnChange, triggerOnInput) => {
+		var _element = $(elementId)
+			.find('input[type="text"], input[type="number"]')
+			.first();
+		var forceInteger = $(elementId).data('forceinteger') === 'True' ? true : false;
+		var currentValue = parseFloat(_element.val());
+		var incrementUnit = 1;
+		var isDecimals = currentValue % 1 != 0;
+
+		$(elementId)
+			.find('.MinusVertical')
+			.removeClass('DisabledSpin');
+
+		if (parseFloat(currentValue) < minValue) {
+			$(elementId)
+				.find('a.MinusVertical')
+				.addClass('DisabledSpin');
+		} else {
+			$(elementId)
+				.find('a.MinusVertical')
+				.removeClass('DisabledSpin');
+		}
+
+		if (!forceInteger) {
+			if (isDecimals) {
+				incrementUnit = parseFloat(0.1);
+			}
+		}
+		if (currentValue === undefined || currentValue === '' || isNaN(parseFloat(currentValue))) {
+			_element.val(minValue);
+			if (triggerOnChange) {
+				_element.trigger('change');
+			}
+			if (triggerOnInput) {
+				_element.trigger('input');
+			}
+		} else {
+			if (parseFloat(currentValue) < maxValue) {
+				if (currentValue === 0 && !forceInteger) {
+					incrementUnit = parseFloat(0.1);
+				}
+				_element.val(parseFloat((currentValue + incrementUnit).toFixed(1)));
+				if (triggerOnChange) {
+					_element.trigger('change');
+				}
+				if (triggerOnInput) {
+					_element.trigger('input');
+				}
+				$(elementId)
+					.find('a.MinusVertical')
+					.removeAttr('disabled');
+			} else {
+				$(elementId)
+					.find('a.PlusVertical')
+					.attr('disabled', 'disabled');
+			}
+		}
+
+		checkSpinnerVerticalDisabledStatus(elementId, parseFloat(_element.val()), minValue, maxValue);
+	};
+
+	const decrement = (elementId, minValue, maxValue, triggerOnChange, triggerOnInput) => {
+		var _element = $(elementId)
+			.find('input[type="text"], input[type="number"]')
+			.first();
+		var forceInteger = $(elementId).data('forceinteger') === 'True' ? true : false;
+		var currentValue = parseFloat(_element.val());
+		var incrementUnit = 1;
+		var isDecimals = currentValue % 1 != 0;
+		if (!forceInteger) {
+			if (isDecimals) {
+				incrementUnit = parseFloat(0.1);
+			}
+		}
+		if (currentValue === undefined || currentValue === '' || isNaN(parseFloat(currentValue))) {
+			_element.val(minValue);
+			if (triggerOnChange) {
+				_element.trigger('change');
+			}
+			if (triggerOnInput) {
+				_element.trigger('input');
+			}
+		} else {
+			if (parseFloat(currentValue) > minValue) {
+				if (currentValue === 1 && !forceInteger) {
+					incrementUnit = parseFloat(0.1);
+				}
+				_element.val(parseFloat((currentValue - incrementUnit).toFixed(1)));
+				if (triggerOnChange) {
+					_element.trigger('change');
+				}
+				if (triggerOnInput) {
+					_element.trigger('input');
+				}
+				$(elementId)
+					.find('a.PlusVertical')
+					.removeAttr('disabled');
+			} else {
+				$(elementId)
+					.find('a.MinusVertical')
+					.attr('disabled', 'disabled');
+			}
+		}
+
+		checkSpinnerVerticalDisabledStatus(elementId, parseFloat(_element.val()), minValue, maxValue);
+	};
+
+	const checkSpinnerVerticalDisabledStatus = (elementId, valueToCheck, minValue, maxValue) => {
+		if (valueToCheck <= minValue) {
+			$(elementId)
+				.find('a.MinusVertical')
+				.attr('disabled', 'disabled');
+		} else {
+			$(elementId)
+				.find('a.MinusVertical')
+				.removeAttr('disabled');
+		}
+		if (valueToCheck >= maxValue) {
+			$(elementId)
+				.find('a.PlusVertical')
+				.attr('disabled', 'disabled');
+		} else {
+			$(elementId)
+				.find('a.PlusVertical')
+				.removeAttr('disabled');
+		}
+	};
+
+	SapphireWidgets.SpinnerVertical = {
+		create,
+		increment,
+		decrement,
+	};
+})(jQuery, window, document, SapphireWidgets);
