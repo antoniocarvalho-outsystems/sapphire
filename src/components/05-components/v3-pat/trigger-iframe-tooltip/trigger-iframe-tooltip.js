788570fe86ded1ffd04daa5fa8336914077ca771
Pablo Santos <pablo.santos@outsystems.com>
2020-04-09 17:18:32 +0100

    fix: small adjusments

--- src/components/05-components/v3-pat/trigger-iframe-tooltip/trigger-iframe-tooltip.js
+++ src/components/05-components/v3-pat/trigger-iframe-tooltip/trigger-iframe-tooltip.js
@@ -1,42 +1,40 @@
 /* Component TriggerIframeTooltip */
+(function($, window, document, SapphireWidgets) {
+	var create = function(config) {
+		var $elementId = $('#' + config.elementId);
+		$elementId.addClass('tooltip');
 
-(function ($, window, document, SapphireWidgets) {
+		var extraDataParams = 'data-iframetooltiptriggerid="' + config.elementId + '"';
+		var widgetNotifyDiv = $.find('[data-iframetooltiptriggerid="' + config.elementId + '"]');
+		if (widgetNotifyDiv.length === 1) {
+			extraDataParams += ' data-iframetooltipnotifyid=' + $(widgetNotifyDiv).data('iframetooltipnotifyid');
+		}
 
-  var create = function (config) {
-    var $elementId = $('#' + config.elementId);
-    $elementId.addClass('tooltip');
+		$elementId.tooltipster({
+			contentAsHTML: true,
+			theme: config.theme,
+			interactive: true,
+			position: config.positionId,
+			trigger: config.triggerId,
+			minWidth: config.minWidth,
+			maxWidth: config.maxWidth,
+			zindex: config.zindex,
+			content:
+				'<iframe src="' + config.URL + '" style="border:none" id="tooltipster-frame" ' + extraDataParams + '></iframe>',
+			functionReady: function(instance, helper) {
+				$(helper).css({
+					visibility: 'hidden',
+				});
+				setTimeout(function() {
+					$('.tooltipster-base').css({
+						visibility: 'visible',
+					});
+				}, 500);
+			},
+		});
+	};
 
-    var extraDataParams = 'data-iframetooltiptriggerid="' + config.elementId + '"';
-    var widgetNotifyDiv = $.find('[data-iframetooltiptriggerid="' + config.elementId + '"]');
-    if (widgetNotifyDiv.length === 1) {
-      extraDataParams += " data-iframetooltipnotifyid=" + $(widgetNotifyDiv).data('iframetooltipnotifyid');
-    }
-
-    $elementId.tooltipster({
-      contentAsHTML: true,
-      theme: config.theme,
-      interactive: true,
-      position: config.positionId,
-      trigger: config.triggerId,
-      minWidth: config.minWidth,
-      maxWidth: config.maxWidth,
-      zindex: config.zindex,
-      content: '<iframe src="' + config.URL + '" style="border:none" id="tooltipster-frame" ' + extraDataParams + '></iframe>',
-      functionReady: function (instance, helper) {
-        $(helper).css({
-          'visibility': 'hidden'
-        });
-        setTimeout(function () {
-          $('.tooltipster-base').css({
-            'visibility': 'visible'
-          });
-        }, 500);
-      }
-    });
-  }
-
-  SapphireWidgets.TriggerIframeTooltip = {
-    create
-  }
-
+	SapphireWidgets.TriggerIframeTooltip = {
+		create,
+	};
 })(jQuery, window, document, SapphireWidgets);
