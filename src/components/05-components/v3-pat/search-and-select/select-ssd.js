10b118fe26158ca0829c5ba1dd9f1b226a444c55
Pablo Santos <pablo.santos@outsystems.com>
2020-04-22 13:39:42 +0100

    feat: search and select scripts

---
+++ src/components/05-components/v3-pat/search-and-select/select-ssd.js
@@ -0,0 +1,126 @@
+var SearchSelectDefine = (window.SearchSelectDefine = window.SearchSelectDefine || {});
+
+SapphireWidgets.SelectSSD = function SSDSelectSetup(config) {
+	$(function() {
+		var $SSDselectId = $('#' + config.SSDSelectId);
+		var isMultiple = config.isMultiple;
+
+		var $ComponentSSD = $SSDselectId.closest('.SearchSD');
+		var $ComponentSSDinput = $ComponentSSD.find('.SearchSD___input input');
+		var Componentinputlength = $ComponentSSDinput.val().length;
+
+		if (Componentinputlength > 0) {
+			$SSDselectId.find('.SelectSD__contentTitle').highlight($ComponentSSDinput.val(), {
+				className: 'SelectSD-searched-term',
+				caseSensitive: false,
+				wordsOnly: false,
+			});
+		}
+
+		var OpenConfirmPopup = function($SSDselectId) {
+			$ComponentSSD = $SSDselectId.closest('.SearchSD');
+			$PopupID = $ComponentSSD.siblings('.SSDPopupWrapper');
+
+			$PopupID.fadeIn('fast', function() {
+				$ComponentSSD.addClass('Dont_Close');
+				$PopupID
+					.find('.SSDpopupOk')
+					.off('click')
+					.on('click', function() {
+						$PopupID.fadeOut('fast', function() {
+							$SSDselectId.find('.SelectSD__starTrigger > a').click();
+							setTimeout($ComponentSSD.removeClass('Dont_Close'), 500);
+						});
+					});
+
+				$PopupID
+					.find('.SSDpopupCancel')
+					.off('click')
+					.on('click', function() {
+						$PopupID.fadeOut('fast', function() {
+							setTimeout($ComponentSSD.removeClass('Dont_Close'), 500);
+						});
+					});
+			});
+		};
+
+		var SSDCheckBoxSelect = function($widgetId) {
+			var checkBoxCount = 0;
+			if (isMultiple === 'True') {
+				checkBoxCount = $widgetId
+					.closest('.SearchSD.showFavorite')
+					.find('.SelectSD__multiple > input[type="checkbox"]:checked').length;
+				console.log(checkBoxCount);
+				$allListcard = $widgetId.closest('.SearchSD_content');
+
+				if (checkBoxCount > 0) {
+					$widgetId.closest('.SearchSD.showFavorite').addClass('MultiSelectActive');
+					$widgetId.closest('.SearchSD_content .SelectSD').each(function() {
+						$(this)
+							.find('.SelectSD_contentWrapper')
+							.off('click');
+						$(this)
+							.find('.SelectSD_actionLink')
+							.off('click');
+					});
+				} else {
+					$widgetId.closest('.SearchSD.showFavorite').removeClass('MultiSelectActive');
+					$widgetId.closest('.SearchSD_content .SelectSD ').each(function() {
+						$(this)
+							.find('.SelectSD_contentWrapper')
+							.on('click', function(e) {
+								console.log('click 6 .SelectSD_contentWrapper', e);
+								$(this)
+									.find('.LineActionLINK > a')
+									.click();
+							});
+						$(this)
+							.find('.SelectSD_actionLink')
+							.on('click', function(e) {
+								console.log('click 5 .SelectSD_actionLink', e);
+								$(this)
+									.find('.LineActionLINK > a')
+									.click();
+							});
+					});
+				}
+			}
+		};
+
+		if (isMultiple === 'True') {
+			console.log($('.SelectSD__multiple'));
+			$SSDselectId.find('.SelectSD__multiple > input').click(function() {
+				console.log('click 4', this);
+				SSDCheckBoxSelect($SSDselectId);
+			});
+		}
+
+		$SSDselectId.find('.SelectSD__starLink').on('click', function(e) {
+			if (!$SSDselectId.find('.SelectSD .SelectSD__starWrapper').hasClass('starDisabled')) {
+				if ($SSDselectId.find('.SelectSD .SelectSD__starWrapper').hasClass('starSelected')) {
+					OpenConfirmPopup($SSDselectId);
+				} else {
+					$SSDselectId.find('.SelectSD__starTrigger > a').click();
+				}
+			}
+		});
+
+		$SSDselectId.find('.SelectSD_contentWrapper').on('click', function(e) {
+			$ComponentSSD = $SSDselectId.closest('.SearchSD');
+			$SSDselectId.find('.LineActionLINK > a').click();
+			if (!$ComponentSSD.hasClass('MultiSelectActive')) {
+				SearchSelectDefine.SSDSearch.returnToSearch($SSDselectId.closest('.SearchSD'));
+				SearchSelectDefine.SSDSearch.closeSSD($SSDselectId.closest('.SearchSD'));
+			}
+		});
+
+		$SSDselectId.find('.SelectSD_actionLink').on('click', function(e) {
+			$ComponentSSD = $SSDselectId.closest('.SearchSD');
+			$SSDselectId.find('.LineActionLINK > a').click();
+			if (!$ComponentSSD.hasClass('MultiSelectActive')) {
+				SearchSelectDefine.SSDSearch.returnToSearch($SSDselectId.closest('.SearchSD'));
+				SearchSelectDefine.SSDSearch.closeSSD($SSDselectId.closest('.SearchSD'));
+			}
+		});
+	});
+};
