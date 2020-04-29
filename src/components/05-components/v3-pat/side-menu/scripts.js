10a1a47d09b5147c06d154524377f67fa82124c5
Pablo Santos <pablo.santos@outsystems.com>
2020-04-29 18:47:06 +0100

    feat: ux enhancements

--- src/components/05-components/v3-pat/side-menu/scripts.js
+++ src/components/05-components/v3-pat/side-menu/scripts.js
@@ -7,28 +7,46 @@
 			this.onComponentInit();
 		}
 
+		openCloseMenu(toOpen) {
+			if (toOpen) {
+				this.$component.addClass('SideMenu--open');
+
+				$('.LayoutBase-iframesidebar').css({ zIndex: 0 });
+			} else {
+				this.$component.removeClass('SideMenu--open');
+
+				$('.LayoutBase-iframesidebar').css({ zIndex: 70 });
+			}
+		}
+
 		onComponentInit() {
 			this.$component = $(`#${this.options.widgetId}`);
 			this.$trigger = this.$component.find('.SideMenu__Trigger');
 			this.$shield = this.$component.find('.SideMenu__Shield');
 			this.$closeButton = this.$component.find('.SideMenu__MenuClose');
+			this.$tabItem = this.$component.find('.SideMenu__TabItems .MenuItem');
 			this.$menuItem = this.$component.find('.SideMenu__MenuItems .MenuItem');
+			this.$subItem = this.$component.find('.SideMenu__MenuItems .MenuItem_subItems');
 
-			this.$trigger.on('click', () => {
-				this.$component.addClass('SideMenu--open');
-			});
+			this.$trigger.on('click', () => this.openCloseMenu(true));
+			this.$shield.on('click', () => this.openCloseMenu(false));
+			this.$closeButton.on('click', () => this.openCloseMenu(false));
 
-			this.$shield.on('click', () => {
-				this.$component.removeClass('SideMenu--open');
-			});
+			this.$tabItem.on('click', event => {
+				const $target = $(event.currentTarget);
+				const $link = $target.find('.MenuItem_label a');
 
-			this.$closeButton.on('click', () => {
-				this.$component.removeClass('SideMenu--open');
+				if ($link.length) $link.get(0).click();
 			});
 
 			this.$menuItem.on('click', event => {
+				if (event.target !== event.currentTarget) return;
+
 				const $target = $(event.currentTarget);
 				const $subItems = $target.find('.MenuItem_subItems');
+				const $link = $target.find('.MenuItem_label a');
+
+				if ($link.length) $link.get(0).click();
 
 				this.$component
 					.find('.SideMenu__MenuItems .active')
@@ -43,6 +61,15 @@
 				$target.toggleClass('active');
 				$subItems.toggleClass('show');
 			});
+
+			this.$subItem.on('click', event => {
+				event.stopPropagation();
+			});
+
+			this.$component
+				.find('.SideMenu__TabItems > div:empty')
+				.parent()
+				.hide();
 		}
 	}
 
