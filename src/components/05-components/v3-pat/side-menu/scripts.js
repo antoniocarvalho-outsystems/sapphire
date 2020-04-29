90992eba3ac02d8165ec2c241617e9d975dd94c0
Pablo Santos <pablo.santos@outsystems.com>
2020-04-29 11:31:40 +0100

    feat: side-menu

--- src/components/05-components/v3-pat/side-menu/scripts.js
+++ src/components/05-components/v3-pat/side-menu/scripts.js
@@ -12,6 +12,7 @@
 			this.$trigger = this.$component.find('.SideMenu__Trigger');
 			this.$shield = this.$component.find('.SideMenu__Shield');
 			this.$closeButton = this.$component.find('.SideMenu__MenuClose');
+			this.$menuItem = this.$component.find('.SideMenu__MenuItems .MenuItem');
 
 			this.$trigger.on('click', () => {
 				this.$component.addClass('SideMenu--open');
@@ -23,6 +24,24 @@
 
 			this.$closeButton.on('click', () => {
 				this.$component.removeClass('SideMenu--open');
+			});
+
+			this.$menuItem.on('click', event => {
+				const $target = $(event.currentTarget);
+				const $subItems = $target.find('.MenuItem_subItems');
+
+				this.$component
+					.find('.SideMenu__MenuItems .active')
+					.not($target)
+					.removeClass('active');
+
+				this.$component
+					.find('.SideMenu__MenuItems .show')
+					.not($target)
+					.removeClass('show');
+
+				$target.toggleClass('active');
+				$subItems.toggleClass('show');
 			});
 		}
 	}
