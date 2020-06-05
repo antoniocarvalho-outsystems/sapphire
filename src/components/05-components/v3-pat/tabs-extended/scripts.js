88f90c5d78363cddc79ac7c9397e283ef66d8d81
Antonio Carvalho <antonio.carvalho@outsystems.com>
2020-06-05 09:51:37 +0100

    listlineserverside fixes

--- src/components/05-components/v3-pat/tabs-extended/scripts.js
+++ src/components/05-components/v3-pat/tabs-extended/scripts.js
@@ -1,6 +1,6 @@
 /* Component TabsExtended */
-SapphireWidgets.TabsExtended = function() {
-	$('.Tabs_Extended .Tabs_header > .Tabs__tab').each(function() {
+SapphireWidgets.TabsExtended = function () {
+	$('.Tabs_Extended .Tabs_header > .Tabs__tab').each(function () {
 		if ($(this).text() === '') {
 			$(this).remove();
 		}
@@ -8,14 +8,14 @@ SapphireWidgets.TabsExtended = function() {
 
 	$('.Tabs_Extended .Tabs_header .Tabs__tab:not(.active)')
 		.off('mousedown')
-		.on('mousedown', function(evt) {
+		.on('mousedown', function (evt) {
 			var $tabsExtended = $(evt.target).closest('.Tabs_Extended');
 			$tabsExtended.removeClass('isClosed');
 		});
 
 	$('.Tabs_Extended.HideActiveOnClick .Tabs_header')
 		.off('mousedown')
-		.on('mousedown', '.Tabs__tab.active', function(evt) {
+		.on('mousedown', '.Tabs__tab.active', function (evt) {
 			var $tabsExtended = $(evt.target).closest('.Tabs_Extended');
 			var $tabsBody = $tabsExtended.find('.Tabs_body');
 
@@ -28,14 +28,14 @@ SapphireWidgets.TabsExtended = function() {
 			}
 		});
 
-	$('.Tabs_Extended--disabled').each(function(index, el) {
+	$('.Tabs_Extended--disabled').each(function (index, el) {
 		$(el)
 			.parent()
 			.css('cursor', 'default')
 			.off('click');
 	});
 
-	$('.Tabs_Extended').each(function(index, el) {
+	$('.Tabs_Extended').each(function (index, el) {
 		if ($(el).hasClass('Tabs_Extended--closedonstart')) {
 			$(el)
 				.find('.Tabs_body')
@@ -45,7 +45,7 @@ SapphireWidgets.TabsExtended = function() {
 		}
 		$(el)
 			.off('click')
-			.on('click', '.Tabs_Extended--close', function(evt) {
+			.on('click', '.Tabs_Extended--close', function (evt) {
 				$(evt.target)
 					.closest('.Tabs_body')
 					.addClass('Tabs_body--closed');
@@ -53,7 +53,7 @@ SapphireWidgets.TabsExtended = function() {
 	});
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
 	SapphireWidgets.TabsExtended();
 	osAjaxBackend.BindAfterAjaxRequest(SapphireWidgets.TabsExtended);
 });
