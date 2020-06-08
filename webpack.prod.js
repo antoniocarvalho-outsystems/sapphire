43b6778cd311b7c626237de8a27f6ccf19183f49
Antonio Carvalho <antonio.carvalho@outsystems.com>
2020-06-08 11:44:35 +0100

    update webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,15 @@
 const merge = require('webpack-merge');
-const webpack = require('webpack');
-const package = require('./package.json');
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	mode: 'production',
+	mode: 'development',
 	output: {
 		filename: 'prod.[name].js',
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: 'prod.[name].css',
-		}),
-		new webpack.BannerPlugin({
-			banner: `Generated: ${new Date()} || Version: ${package.version}`,
 		}),
 	],
 });
