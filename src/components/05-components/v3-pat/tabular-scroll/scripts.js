905379a6a63bd9c070862380053c6bb801accb37
Pablo Santos <pablo.santos@outsystems.com>
2020-06-26 19:05:47 +0100

    tabular-scroll

---
+++ src/components/05-components/v3-pat/tabular-scroll/scripts.js
@@ -0,0 +1,58 @@
+/* Component TabularScroll */
+(function($, window, document, SapphireWidgets) {
+	const create = function(config) {
+		$(document).ready(function() {
+			$('.TabularScroll').each(function(i, el) {
+				setupTabularScroll($(el));
+			});
+		});
+
+		$(window).load(function() {
+			osAjaxBackend.BindAfterAjaxRequest(function() {
+				$('.TabularScroll').each(function(i, el) {
+					setupTabularScroll($(el));
+				});
+			});
+		});
+
+		$(window).on('resize.tabularscroll', function() {
+			$('.TabularScroll').each(function(i, el) {
+				var $centerTable = $(el).find('.TabularScroll-center-table');
+				var tableWidth = $centerTable.find('table').width();
+				$(el)
+					.find('.TabularScroll-center .TopScrollDragger')
+					.width(tableWidth);
+				if ($centerTable[0].scrollWidth > $centerTable.width()) {
+					$(el)
+						.find('.TopScrollWrapper')
+						.css('visibility', 'visible');
+				} else {
+					$(el)
+						.find('.TopScrollWrapper')
+						.css('visibility', 'hidden');
+				}
+			});
+		});
+	};
+
+	const setupTabularScroll = function($tabularScroll) {
+		var $topScrollWrapper = $tabularScroll.find('.TopScrollWrapper');
+		var $centerTable = $tabularScroll.find('.TabularScroll-center-table');
+		$tabularScroll.find('.TopScrollWrapper').scroll(function() {
+			$centerTable.scrollLeft($topScrollWrapper.scrollLeft());
+		});
+		$centerTable.scroll(function() {
+			$topScrollWrapper.scrollLeft($centerTable.scrollLeft());
+		});
+		// similar to Resize
+		var tableWidth = $centerTable.find('table').width();
+		$tabularScroll.find('.TabularScroll-center .TopScrollDragger').width(tableWidth);
+		if ($centerTable[0].scrollWidth > $centerTable.width()) {
+			$tabularScroll.find('.TopScrollWrapper').css('visibility', 'visible');
+		} else {
+			$tabularScroll.find('.TopScrollWrapper').css('visibility', 'hidden');
+		}
+	};
+
+	SapphireWidgets.TabularScroll = { create };
+})(jQuery, window, document, SapphireWidgets);
