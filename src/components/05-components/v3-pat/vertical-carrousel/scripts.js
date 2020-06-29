fbb59c1f7e935b1f0fc905bf79daafca491387c8
Pablo Santos <pablo.santos@outsystems.com>
2020-06-29 16:00:29 +0100

    vertical-carousel

---
+++ src/components/05-components/v3-pat/vertical-carrousel/scripts.js
@@ -0,0 +1,144 @@
+/* Component VerticalCarousel */
+(function($, window, document, SapphireWidgets) {
+	const create = function() {
+		$.fn.verticalCarousel = function(options) {
+			var carouselContainerClass = 'VerticalCarousel_Wrapper';
+			var carouselGroupClass = 'VerticalCarousel__List';
+			var carouselGoUpClass = 'VerticalCarousel___ChangeUp';
+			var carouselGoDownClass = 'VerticalCarousel___ChangeDown';
+
+			var defaults = { currentItem: 1, showItems: 1 };
+			var options = $.extend(defaults, options);
+
+			var carouselContainer;
+			var carouselGroup;
+			var carouselUp;
+			var carouselDown;
+			var totalItems;
+
+			var setContainerHeight = function() {
+				var containerHeight = 0;
+				var marginTop = 0;
+				if (options.showItems == 1) {
+					containerHeight = $(' > span > div:nth-child(' + options.currentItem + ')', carouselGroup).outerHeight(true);
+				} else {
+					for (i = 1; i == options.showItems; i++) {
+						containerHeight = containerHeight + $(' > div:nth-child(' + i + ')', carouselGroup).outerHeight(true);
+					}
+				}
+				var nextItem = options.showItems + options.currentItem;
+				marginTop = $(' > span > div:nth-child(' + nextItem + ')', carouselGroup).css('marginTop');
+				containerHeight = containerHeight - parseInt(marginTop);
+				$(carouselContainer).css({ height: containerHeight });
+			};
+
+			var setOffset = function() {
+				var currentItemOffset = $(' > span > div:nth-child(' + options.currentItem + ')', carouselGroup).offset();
+
+				var carouselGroupOffset = $(carouselGroup).offset();
+				var offsetDiff = carouselGroupOffset.top - currentItemOffset.top;
+
+				$('.VerticalCarousel__List .PrescriptionCard').css('background-color', '#DAE0E4');
+
+				$(carouselGroup).css({
+					msTransform: 'translateY(calc(36% + ' + offsetDiff + 'px))',
+					webkitTransform: 'translateY(calc(36% + ' + offsetDiff + 'px))',
+					transform: 'translateY(calc(36% + ' + offsetDiff + 'px))',
+				});
+				$(' > span > div:nth-child(' + options.currentItem + ')', carouselGroup).css('background-color', '#fff');
+			};
+
+			var fetchCard = function() {
+				if ($('#' + CardId)) {
+					currentItem =
+						$('#' + CardId)
+							.parent()
+							.index() + 1;
+				}
+			};
+
+			var updateNavigation = function(direction) {
+				if (options.currentItem == 1) {
+					$(carouselUp).addClass('isDisabled');
+				} else if (options.currentItem > 1) {
+					$(carouselUp).removeClass('isDisabled');
+				}
+				if (options.currentItem == totalItems || options.currentItem + options.showItems > totalItems) {
+					$(carouselDown).addClass('isDisabled');
+				} else if (options.currentItem < totalItems) {
+					$(carouselDown).removeClass('isDisabled');
+				}
+			};
+
+			var moveCarousel = function(direction) {
+				if (direction == 'up') {
+					options.currentItem = options.currentItem - 1;
+				}
+				if (direction == 'down') {
+					options.currentItem = options.currentItem + 1;
+				}
+				updateNavigation();
+				setContainerHeight();
+				setOffset();
+			};
+
+			return this.each(function() {
+				$(this)
+					.find('.' + carouselGroupClass + ', .VerticalCarousel__Controllers')
+					.wrapAll('<div class="' + carouselContainerClass + '"></div>');
+				carouselContainer = $(this).find('.' + carouselContainerClass);
+				carouselGroup = $(this).find('.' + carouselGroupClass);
+				carouselUp = $(this).find('.' + carouselGoUpClass);
+				carouselDown = $(this).find('.' + carouselGoDownClass);
+				totalItems = $('.' + carouselGroupClass + ' > span').children().length;
+				updateNavigation();
+				setContainerHeight();
+				setOffset();
+				$(carouselUp).on('click', function(e) {
+					if (options.currentItem > 1) {
+						moveCarousel('up');
+					}
+					e.preventDefault();
+				});
+				$(carouselDown).on('click', function(e) {
+					if (options.currentItem + options.showItems <= totalItems) {
+						moveCarousel('down');
+					}
+					e.preventDefault();
+				});
+
+				$('.VerticalCarousel.Open').bind('mousewheel', function(e) {
+					if (e.originalEvent.wheelDelta / 120 > 0) {
+						if (options.currentItem > 1) {
+							moveCarousel('up');
+						}
+						e.preventDefault();
+					} else {
+						if (options.currentItem + options.showItems <= totalItems) {
+							moveCarousel('down');
+						}
+						e.preventDefault();
+					}
+				});
+			});
+		};
+	};
+
+	const onOpen = function(cardNumber) {
+		$(document).ready(function() {
+			$('.VerticalCarousel').verticalCarousel({
+				currentItem: cardNumber,
+				showItems: 1,
+			});
+
+			$('.Page').css('overflow', 'hidden');
+
+			$('.VerticalCarousel___Close').click(function() {
+				$('.VerticalCarousel').removeClass('Open');
+				$('.Page').css('overflow', 'initial');
+			});
+		});
+	};
+
+	SapphireWidgets.VerticalCarousel = { create, onOpen };
+})(jQuery, window, document, SapphireWidgets);
