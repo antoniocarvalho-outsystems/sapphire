0463f2abad0729ff2c9d4bac51409f3abbf27603
Pablo Santos <pablo.santos@outsystems.com>
2020-06-29 12:03:03 +0100

    card-patient-table

---
+++ src/components/05-components/v3-pat/card-patient-table/scripts.js
@@ -0,0 +1,24 @@
+/* Component CardPatientTable */
+(function($, window, SapphireWidgets) {
+	const create = config => {
+		$(document).ready(function() {
+			$('.CardPatientInfo div a').click(function() {
+				$(this).addClass('Bold');
+				$(this)
+					.parent()
+					.siblings('.ThemeGrid_Width2')
+					.find('a')
+					.removeClass('Bold');
+				$(this)
+					.parent()
+					.parent()
+					.siblings()
+					.children()
+					.find('a')
+					.removeClass('Bold');
+			});
+		});
+	};
+
+	SapphireWidgets.CardPatientTable = { create };
+})(jQuery, window, SapphireWidgets);
