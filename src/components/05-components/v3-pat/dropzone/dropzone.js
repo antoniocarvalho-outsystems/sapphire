ca852185c9da4e256fe24774e2a5ee13ae56184c
Pablo Santos <pablo.santos@outsystems.com>
2020-07-01 15:14:24 +0100

    fix: dropzone

---
+++ src/components/05-components/v3-pat/dropzone/dropzone.js
@@ -0,0 +1,50 @@
+/* Component Dropzone */
+(function($, window, document, SapphireWidgets) {
+	const create = function(config) {
+		$(document).ready(function() {
+			bindEvents(config);
+
+			window.Dropzone.autoDiscover = false;
+
+			const myDropzone = new window.Dropzone(config.hiddenInputContainer, {
+				...config,
+				init: function() {
+					let prevFile;
+
+					if (+config.maxFiles === 1 && config.isReplacePrevious) {
+						this.on('addedfile', function() {
+							if (typeof prevFile !== undefined) {
+								this.removeFile(prevFile);
+							}
+						});
+					}
+
+					const $notifyInput = $(`#${config.notifyInputId}`);
+
+					this.on('success', function(file, responseText) {
+						prevFile = file;
+
+						$(`#${config.notifyInputId} .dz-filename`).attr('title', file.name);
+						$notifyInput.val(responseText);
+						$notifyInput.change();
+					});
+
+					this.on('error', function(file, responseText) {
+						prevFile = file;
+
+						$notifyInput.val(responseText);
+						$notifyInput.change();
+					});
+				},
+			});
+		});
+	};
+
+	const bindEvents = function(config) {
+		$(`#${config.widgetId} .UploadMessageClass`).on('click', () => {
+			$(`#${config.widgetId} .dz-clickable`).click();
+		});
+	};
+
+	SapphireWidgets.Dropzone = { create };
+})(jQuery, window, document, SapphireWidgets);
