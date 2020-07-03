2234a0bdd832099f82938c43a244606ac6b3c5f2
Pablo Santos <pablo.santos@outsystems.com>
2020-07-03 13:03:42 +0100

    country-phone

--- src/components/05-components/v3-pat/country-phone/scripts.js
+++ src/components/05-components/v3-pat/country-phone/scripts.js
@@ -5,7 +5,7 @@
 
 		const countryPhoneInput = window.intlTelInput($element, {
 			initialCountry: config.initialCountry,
-			preferredCountries: preferedCountries,
+			preferedCountries: config.preferedCountries,
 			separateDialCode: config.separateDialCode,
 			nationalMode: config.nationalMode,
 			autoPlaceholder: config.autoPlaceholder ? 'polite' : false,
@@ -16,18 +16,3 @@
 
 	SapphireWidgets.CountryPhone = { create };
 })(jQuery, window, SapphireWidgets);
-
-/*"
-var countryPhone_" + PhoneInput + " = document.querySelector('#" + PhoneInput + "');
-
-var input_" + PhoneInput + " = window.intlTelInput(countryPhone_" + PhoneInput + ", {
-    initialCountry:'" + EncodeJavaScript(initialCountry) + "',
-    " + If(Trim(preferedCountries)="","","preferredCountries:  " + preferedCountries + " ,") + "
-    separateDialCode: " + If(separateDialCode,"true", "false") + ",
-    nationalMode: " + If(nationalMode,"true", "false") + ",
-    autoPlaceholder: " + If(autoPlaceholder,"polite", "false") + ",
-    formatOnDisplay: false,
-    utilsScript: '/V3_Pat/js/utils.js' // just for formatting/placeholders etc
-});
-
-"*/
