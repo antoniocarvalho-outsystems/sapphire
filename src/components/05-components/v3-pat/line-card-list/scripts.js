93dbb6e247dfc7c6c6036c6195c0b51bf97b538d
Pablo Santos <pablo.santos@outsystems.com>
2020-07-07 11:33:58 +0100

    line-card-list

---
+++ src/components/05-components/v3-pat/line-card-list/scripts.js
@@ -0,0 +1,44 @@
+/* Component LineCardList */
+(function($, window, SapphireWidgets) {
+	const create = config => {
+		$(document).ready(function() {
+			const $component = $(`#${config.widgetId}`);
+			const $card = $component.find('.LineCardList');
+			const $checkBox = $component.find('.LineCardList_checkbox label');
+
+			$card.click(function() {
+				$(this).toggleClass('active');
+
+				$card.not(this).removeClass('active');
+
+				$card
+					.not(this)
+					.find('.LineCardList_checkbox input[type=checkbox]')
+					.prop('checked', false);
+
+				if ($(this).hasClass('active')) {
+					$(this)
+						.find('.LineCardList_checkbox input[type=checkbox]')
+						.prop('checked', true);
+				} else {
+					$(this)
+						.find('.LineCardList_checkbox input[type=checkbox]')
+						.prop('checked', false);
+				}
+			});
+
+			$checkBox.click(function() {
+				$card
+					.not(this)
+					.closest('.LineCardList')
+					.removeClass('active');
+
+				$(this)
+					.closest('.LineCardList')
+					.toggleClass('active');
+			});
+		});
+	};
+
+	SapphireWidgets.LineCardList = { create };
+})(jQuery, window, SapphireWidgets);
