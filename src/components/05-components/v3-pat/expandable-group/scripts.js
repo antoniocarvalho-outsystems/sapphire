b0ba4a67946215bdd5f1b4f83196388a6ac982e4
Pablo Santos <pablo.santos@outsystems.com>
2020-07-13 15:05:34 +0100

    expandable-group

---
+++ src/components/05-components/v3-pat/expandable-group/scripts.js
@@ -0,0 +1,72 @@
+/* Component ExpandableGroup */
+(function($, window, document, SapphireWidgets) {
+	var allExpandableGroups = [];
+
+	var create = function(config) {
+		for (var i = 0; i < allExpandableGroups.length; i++) {
+			if (allExpandableGroups[i].config.widgetId === config.widgetId) {
+				allExpandableGroups.splice(i, 1);
+			}
+		}
+		window[config.widgetId] = new ExpandableGroup(config);
+		allExpandableGroups.push(window[config.widgetId]);
+
+		$(window).load(function() {
+			osAjaxBackend.BindAfterAjaxRequest(function() {
+				var allExpandableGroups = SapphireWidgets.ExpandableGroup.all();
+				allExpandableGroups.forEach(function(element) {
+					SapphireWidgets.ExpandableGroup.create(element.config);
+				});
+			});
+		});
+	};
+
+	var ExpandableGroup = function(config) {
+		var _this = this;
+		this.config = config;
+		this.$widget = $('#' + this.config.widgetId);
+		this.$items = this.$widget.find('.ExpandableGroupItem');
+		this.$items.each(function() {
+			window[$(this).attr('id')] = new ExpandableGroupItem($(this), _this);
+		});
+	};
+
+	var ExpandableGroupItem = function($item, group) {
+		var _this = this;
+		this.group = group;
+		this.$widget = $item;
+		this.$content = this.$widget.find('.ExpandableGroupItem-content');
+		this.$widget.off('click').on('click', '.ExpandableGroupItem-header', function() {
+			if (_this.$widget.hasClass('isOpen')) {
+				_this.close();
+			} else {
+				_this.group.closeAll();
+				_this.open();
+			}
+			if (SapphireWidgets.ResizeParentIframe) {
+				SapphireWidgets.ResizeParentIframe.resize();
+			}
+		});
+	};
+
+	ExpandableGroup.prototype.closeAll = function() {
+		this.$items.each(function() {
+			window[$(this).attr('id')].close();
+		});
+	};
+
+	ExpandableGroupItem.prototype.open = function() {
+		this.$widget.addClass('isOpen');
+	};
+
+	ExpandableGroupItem.prototype.close = function() {
+		this.$widget.removeClass('isOpen');
+	};
+
+	SapphireWidgets.ExpandableGroup = {
+		create: create,
+		all: function() {
+			return allExpandableGroups;
+		},
+	};
+})(jQuery, window, document, SapphireWidgets);
