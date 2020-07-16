6bee1dfb4b02239f278c2d691749341a43ba8172
Antonio Carvalho <antonio.carvalho@outsystems.com>
2020-07-16 17:22:40 +0100

    add important into nomargin

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! Version: 5.1.910054 || Generated: Thu Jul 16 2020 14:18:05 GMT+0100 (Hora de verão da Europa Ocidental) */
+/*! Version: 5.1.910054 || Generated: Thu Jul 16 2020 14:20:48 GMT+0100 (Hora de verão da Europa Ocidental) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
