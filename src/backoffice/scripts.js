80a024725670c1c4eae47c3b03e87b973139d81b
Pablo Santos <pablo.santos@outsystems.com>
2020-07-21 13:45:55 +0100

    hour-picker + sapphire-tooltip backoffice

--- src/backoffice/scripts.js
+++ src/backoffice/scripts.js
@@ -1,13 +1,11 @@
 require('./styles.scss');
 
-
 SapphireWidgets = window.SapphireWidgets = window.SapphireWidgets || {};
-
 
 require('./01-components/layout/layout-backoffice');
 
-
 require('../components/05-components/v3-pat/datetime-range-picker/scripts');
+require('../components/05-components/v3-pat/hour-picker/scripts');
 require('../components/05-components/v3-pat/panel/confirmation-popup');
 require('../components/05-components/v3-pat/panel/panel-by-id');
 require('../components/05-components/v3-pat/select-system/scripts');
