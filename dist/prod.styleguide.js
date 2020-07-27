771c58c81fbaa2efdc50d562b1ef0d4464e208f4
Pablo Santos <pablo.santos@outsystems.com>
2020-07-27 14:29:47 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.920026 || Generated: Mon Jul 27 2020 12:53:58 GMT+0100 (Hora de verão da Europa Ocidental) */
+/*! prod.styleguide.js || Version: 5.1.920027 || Generated: Mon Jul 27 2020 14:29:40 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
