867154e0bc7841e876c3fe591391f0c658cf74d5
Pablo Santos <pablo.santos@outsystems.com>
2020-08-06 15:24:59 +0100

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.930001 || Generated: Thu Aug 06 2020 11:34:25 GMT+0100 (Western European Summer Time) */
+/*! prod.app.js || Version: 5.1.930002 || Generated: Thu Aug 06 2020 15:24:50 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.930001 || Generated: Thu Aug 06 2020 11:34:25 GMT+0100 (Western European Summer Time) */
+/*! prod.styleguide.js || Version: 5.1.930002 || Generated: Thu Aug 06 2020 15:24:50 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
