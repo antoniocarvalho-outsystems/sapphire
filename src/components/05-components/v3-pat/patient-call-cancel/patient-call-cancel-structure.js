88c050010f857093705838cc26e229c7a8a8c657
Pablo Santos <pablo.santos@outsystems.com>
2020-08-26 14:13:28 +0100

    patient-call-cancel-structure

--- src/components/05-components/v3-pat/patient-call-cancel/patient-call-cancel-structure.js
+++ src/components/05-components/v3-pat/patient-call-cancel/patient-call-cancel-structure.js
@@ -3,12 +3,14 @@
 	const create = function(config) {
 		const $widget = $('#' + config.widgetId).find('.PatientCallCancelStructure');
 		const $listQueueWrapper = $widget.find('.PatientCallCancelStructure__ListQueues');
-		const $dropdown = $listQueueWrapper.find('.InlineDropdown');
+		const $dropdown = $listQueueWrapper.find('.InlineDropdown_label');
 
-		$listQueueWrapper.on('click', () => {
+		$listQueueWrapper.on('click', event => {
 			if (!$dropdown.length) return;
+			event.stopPropagation();
 
 			$dropdown.trigger('click');
+			event.preventDefault();
 		});
 	};
 
