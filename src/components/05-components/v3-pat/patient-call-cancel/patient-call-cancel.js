446a462486e2fa752d39b2b656e199e5be20b609
Pablo Santos <pablo.santos@outsystems.com>
2020-08-27 14:28:23 +0100

    patient-call-cancel fix

--- src/components/05-components/v3-pat/patient-call-cancel/patient-call-cancel.js
+++ src/components/05-components/v3-pat/patient-call-cancel/patient-call-cancel.js
@@ -24,6 +24,12 @@
 			$countdown.text(config.txtCallPatient);
 		};
 
+		const setInitialState = () => {
+			$widget.removeClass('CallingPatient');
+			$callButton.show();
+			$otherContent.show();
+		};
+
 		const startCounter = () => {
 			timeCounter = config.timeToCancel;
 			interval = window.setInterval(updateCounter, 1000);
@@ -35,6 +41,8 @@
 			if (timeCounter === 0) {
 				clearInterval(interval);
 				OsNotifyWidget(config.patientCallFakeNotifyId, '');
+
+				setInitialState();
 			}
 
 			if (config.showCountdown) $countdown.text(config.txtCallingIn + ' ' + timeCounter);
