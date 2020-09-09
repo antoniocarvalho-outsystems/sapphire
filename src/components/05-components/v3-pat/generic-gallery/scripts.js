43f67478382b9fd3bea328c153189ac449312558
Pablo Santos <pablo.santos@outsystems.com>
2020-09-09 17:27:57 +0100

    ward dashboard page

--- src/components/05-components/v3-pat/generic-gallery/scripts.js
+++ src/components/05-components/v3-pat/generic-gallery/scripts.js
@@ -1,27 +1,29 @@
 /* Component GenericGallery */
-(function ($, window, document, SapphireWidgets) {
-
-	var create = function (config) {
+(function($, window, document, SapphireWidgets) {
+	var create = function(config) {
 		window[config.widgetId] = new GenericGallery(config);
 	};
 
-	var GenericGallery = function (config) {
+	var GenericGallery = function(config) {
 		this.config = config;
 		this.$widget = $('#' + this.config.widgetId).css('display', 'block');
 		this.equalHeight = this.config.equalHeight;
-		if (this.$widget.find('.GenericGallery-content > span').length === 1 && this.$widget.find('.GenericGallery-content > span').hasClass('ListRecords')) {
-			this.$gallery = this.$widget.find('.GenericGallery-content > span.ListRecords');
+		if (
+			this.$widget.find('> .GenericGallery-content > span').length === 1 &&
+			this.$widget.find('.GenericGallery-content > span').hasClass('ListRecords')
+		) {
+			this.$gallery = this.$widget.find('> .GenericGallery-content > span.ListRecords');
 		} else {
-			this.$gallery = this.$widget.find('.GenericGallery-content');
+			this.$gallery = this.$widget.find('> .GenericGallery-content');
 		}
 
 		this.$gallery.css({
-			'display': 'grid',
-			'gridTemplateColumns': 'repeat(' + this.config.columnSizing + ', minmax(' + this.config.columnMinWidth + ', 1fr))'
+			display: 'grid',
+			gridTemplateColumns: 'repeat(' + this.config.columnSizing + ', minmax(' + this.config.columnMinWidth + ', 1fr))',
 		});
 
 		this.$galleryItems = this.$gallery.children();
-		this.$galleryItems.each(function () {
+		this.$galleryItems.each(function() {
 			if (!$(this).hasClass('GenericGallery-item')) {
 				$(this).wrap('<div class="GenericGallery-item"></div>');
 			}
@@ -29,7 +31,6 @@
 	};
 
 	SapphireWidgets.GenericGallery = {
-		create: create
+		create: create,
 	};
-
 })(jQuery, window, document, SapphireWidgets);
