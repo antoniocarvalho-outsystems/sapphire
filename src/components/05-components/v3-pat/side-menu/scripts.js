bcb0e61785be22ddf591e9eb89039e5ddbea5fe5
Pablo Santos <pablo.santos@outsystems.com>
2020-10-23 17:47:49 +0100

    ui adjustments

--- src/components/05-components/v3-pat/side-menu/scripts.js
+++ src/components/05-components/v3-pat/side-menu/scripts.js
@@ -1,9 +1,9 @@
 /* Component SideMenuStructure */
-(function ($, window, SapphireWidgets) {
+(function($, window, SapphireWidgets) {
 	class SideMenu {
 		constructor(config) {
 			this.options = {
-				...config
+				...config,
 			};
 
 			this.onComponentInit();
@@ -17,7 +17,7 @@
 				if ($sideBarIframe.length && !$layoutHasTabs.length) {
 					const $mainMenu = $('.LayoutBase-MainMenu');
 					$mainMenu.css({
-						width: 'calc(100% - 262px)'
+						width: 'calc(100% - 262px)',
 					});
 				}
 			});
@@ -28,13 +28,13 @@
 				this.$component.addClass('SideMenu--open');
 
 				$('.LayoutBase-iframesidebar').css({
-					zIndex: 0
+					zIndex: 0,
 				});
 			} else {
 				this.$component.removeClass('SideMenu--open');
 
 				$('.LayoutBase-iframesidebar').css({
-					zIndex: 70
+					zIndex: 70,
 				});
 			}
 		}
@@ -47,16 +47,14 @@
 			this.$shield = this.$component.find('.SideMenu__Shield');
 			this.$closeButton = this.$component.find('.SideMenu__MenuClose');
 			this.$tabItem = this.$component.find('.SideMenu__TabItems .MenuItem');
-			this.$menuItem = this.$component.find('.SideMenu__MenuItems .MenuItem');
+			this.$menuItem = this.$component.find('.SideMenu__MenuItems .MenuItem__ItemTitle');
 			this.$subItem = this.$component.find('.SideMenu__MenuItems .MenuItem_subItems');
-
 
 			this.$iframeContainer = this.$component.find('.iframeContainer');
 			this.$iframeContainer.append('<div class="lds-ring"><div></div><div></div><div></div><div></div></div>');
 			this.$iframeContainer.find('iframe').load(() => {
 				this.$iframeContainer.find('.lds-ring').fadeOut();
 			});
-
 
 			this.$trigger.on('click', () => this.openCloseMenu(true));
 			this.$shield.on('click', () => this.openCloseMenu(false));
@@ -72,7 +70,7 @@
 			this.$menuItem.on('click', event => {
 				if (event.target !== event.currentTarget) return;
 
-				const $target = $(event.currentTarget);
+				const $target = $(event.currentTarget).parent();
 				const $subItems = $target.find('.MenuItem_subItems');
 				const $link = $target.find('.MenuItem_label a');
 
@@ -104,7 +102,6 @@
 	const create = config => (window[config.widgetId] = new SideMenu(config));
 
 	SapphireWidgets.SideMenu = {
-		create
+		create,
 	};
-
 })(jQuery, window, SapphireWidgets);
