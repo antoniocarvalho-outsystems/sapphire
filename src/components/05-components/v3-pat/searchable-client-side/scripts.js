5ee01e12464a81b2abac2b13b12467bb93953ec0
Pablo Santos <pablo.santos@outsystems.com>
2020-10-28 15:55:41 +0000

    feat: new option on ssd-search

--- src/components/05-components/v3-pat/searchable-client-side/scripts.js
+++ src/components/05-components/v3-pat/searchable-client-side/scripts.js
@@ -1,64 +1,82 @@
 /* Component SearchClientSide */
+(function($, window, SapphireWidgets) {
+	class SearchClientSide {
+		constructor(config) {
+			this.options = {
+				...config,
+			};
 
-(function ($, window, SapphireWidgets) {
+			this.onComponentInit();
 
-  class SearchClientSide {
+			$(window).load(() => {
+				osAjaxBackend.BindAfterAjaxRequest(() => {
+					setTimeout(() => {
+						$('#' + this.options.inputId).trigger('change');
+					}, 200);
+				});
+			});
+		}
 
-    constructor(config) {
-      this.options = {
-        ...config,
-      };
+		onComponentInit() {
+			$('#' + this.options.inputId).on('change keydown paste input', e => {
+				if (e.keyCode === 13) {
+					e.preventDefault();
+				} else {
+					this.searchClientSide(
+						this.options.inputId,
+						this.options.searchableElementSelector,
+						this.options.searchableChildSelector
+					);
+				}
+			});
+		}
 
-      this.onComponentInit();
+		searchClientSide(inputId, selector, childSelector) {
+			if ($('#' + inputId).is(':visible')) {
+				var searchText = osjs('#' + inputId)
+					.val()
+					.toLowerCase();
+				var searchCounter = 0;
+				var selecion = osjs(selector);
 
-      $(window).load(() => {
-        osAjaxBackend.BindAfterAjaxRequest(() => {
-          setTimeout(() => {
-            $('#' + this.options.inputId).trigger('change');
-          }, 200);
-        });
-      });
+				selecion.removeClass('searchNotFound');
+				selecion.removeClass('searchFound');
+				selecion.unhighlight();
 
-    }
+				if (childSelector) {
+					const elementToSearch = osjs(childSelector);
+					elementToSearch.unhighlight();
+				}
 
-    onComponentInit() {
-      $('#' + this.options.inputId).on('change keydown paste input', (e) => {
-        if (e.keyCode === 13) {
-          e.preventDefault();
-        } else {
-          this.searchClientSide(this.options.inputId, this.options.searchableElementSelector);
-        }
-      });
-    }
+				if (searchText.length > 1) {
+					selecion.each(function() {
+						const textToCompare = childSelector
+							? $(this)
+									.find(childSelector)
+									.text()
+							: this.innerText;
 
-    searchClientSide(inputId, selector) {
-      if ($('#' + inputId).is(':visible')) {
-        var searchText = osjs('#' + inputId).val().toLowerCase();
-        var searchCounter = 0;
-        var selecion = osjs(selector);
-        selecion.removeClass('searchNotFound');
-        selecion.removeClass('searchFound');
-        selecion.unhighlight();
-        if (searchText.length > 1) {
-          selecion.each(function () {
-            if (this.innerText.trim().toLowerCase().indexOf(searchText) > -1) {
-              osjs(this).addClass('searchFound');
-              searchCounter++;
-              osjs(this).highlight(searchText);
-            } else {
-              osjs(this).addClass('searchNotFound');
-            }
-          });
-        }
-      }
-    }
+						if (
+							textToCompare
+								.trim()
+								.toLowerCase()
+								.indexOf(searchText) > -1
+						) {
+							osjs(this).addClass('searchFound');
+							searchCounter++;
+							osjs(this).highlight(searchText);
+						} else {
+							osjs(this).addClass('searchNotFound');
+						}
+					});
+				}
+			}
+		}
+	}
 
-  }
+	const create = config => (window[config.widgetId] = new SearchClientSide(config));
 
-  const create = config => (window[config.widgetId] = new SearchClientSide(config));
-
-  SapphireWidgets.SearchClientSide = {
-    create,
-  };
-
+	SapphireWidgets.SearchClientSide = {
+		create,
+	};
 })(jQuery, window, SapphireWidgets);
