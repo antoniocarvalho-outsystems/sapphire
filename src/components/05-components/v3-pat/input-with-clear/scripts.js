767cd708ddff2b326e756ea6db019a4213ed03a8
Pablo Santos <pablo.santos@outsystems.com>
2020-10-28 17:53:39 +0000

    feat: new option on ssd-search

--- src/components/05-components/v3-pat/input-with-clear/scripts.js
+++ src/components/05-components/v3-pat/input-with-clear/scripts.js
@@ -1,54 +1,52 @@
-(function () {
+(function() {
+	class InputWithClear {
+		constructor(config) {
+			this.config = config;
+			this.$widget = $(`#${config.widgetId}`);
+			this.$input = this.$widget.find('input[type]');
+			this.$clear = this.$widget.find('.InputWithClear-clear');
+			this.$notifyLink = this.$widget.find('.InputWithClear-notify-link');
+			this.$widgetShield = this.$widget.find('.InputWithClear--shield');
+			this.onInit();
+		}
 
-  class InputWithClear {
+		onInit() {
+			this.$input.on('keyup', () => {
+				if (this.$input.val() !== '') this.$clear.show();
+				else this.$clear.show();
+			});
+			this.$input.on('blur', () => {
+				if (this.$input.val() === '') {
+					window.setTimeout(() => {
+						if ($('.daterangepicker:visible').length === 1) {
+							return false;
+						}
+						this.$clear.hide();
+						this.$notifyLink.trigger('click');
+					}, 100);
+				}
+			});
+			this.$clear.on('click', () => {
+				this.$input.val('');
+				this.$clear.hide();
+				this.$notifyLink.trigger('click');
+			});
+			if (this.config.hasShield) {
+				window.setTimeout(() => {
+					this.$widgetShield.hide();
+				}, this.config.shieldTimeout);
+				if (this.config.shieldTimeout > 0) {
+					this.$widgetShield.on('click', () => {
+						this.$clear.hide();
+					});
+				}
+			}
+		}
+	}
 
-    constructor(config) {
-      this.config = config;
-      this.$widget = $(`#${config.widgetId}`);
-      this.$input = this.$widget.find('input[type]');
-      this.$clear = this.$widget.find('.InputWithClear-clear');
-      this.$notifyLink = this.$widget.find('.InputWithClear-notify-link');
-      this.$widgetShield = this.$widget.find('.InputWithClear--shield');
-      this.onInit();
-    }
+	const create = config => (window[config.widgetId] = new InputWithClear(config));
 
-    onInit() {
-      this.$input.on('focus', () => {
-        this.$clear.show();
-      });
-      this.$input.on('blur', () => {
-        if (this.$input.val() === '') {
-          window.setTimeout(() => {
-            if ($('.daterangepicker:visible').length === 1) {
-              return false;
-            }
-            this.$clear.hide();
-            this.$notifyLink.trigger('click');
-          }, 100);
-        };
-      });
-      this.$clear.on('click', () => {
-        this.$input.val('');
-        this.$clear.hide();
-        this.$notifyLink.trigger('click');
-      });
-      if (this.config.hasShield) {
-        window.setTimeout(() => {
-          this.$widgetShield.hide();
-        }, this.config.shieldTimeout);
-        if (this.config.shieldTimeout > 0) {
-          this.$widgetShield.on('click', () => {
-            this.$clear.hide();
-          });
-        }
-      }
-    }
-  }
-
-  const create = config => (window[config.widgetId] = new InputWithClear(config));
-
-  SapphireWidgets.InputWithClear = {
-    create
-  };
-
+	SapphireWidgets.InputWithClear = {
+		create,
+	};
 })();
