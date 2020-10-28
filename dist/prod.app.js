f9becab2c39e39966dc6be2607b36c45f8ed3d0d
Pablo Santos <pablo.santos@outsystems.com>
2020-10-28 12:58:31 +0000

    New Release

--- dist/prod.app.js
+++ dist/prod.app.js
@@ -1,4 +1,4 @@
-/*! prod.app.js || Version: 5.1.980016 || Generated: Mon Oct 26 2020 15:43:22 GMT+0000 (Western European Standard Time) */
+/*! prod.app.js || Version: 5.1.980017 || Generated: Wed Oct 28 2020 12:58:22 GMT+0000 (Western European Standard Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
