1d28f6a5038c910ddff0538d11f65273d94c782c
Pablo Santos <pablo.santos@outsystems.com>
2020-11-12 17:08:24 +0000

    e-signature

--- src/xx_garbage/dummy1.js
+++ src/xx_garbage/dummy1.js
@@ -1,18 +1,29 @@
-let targetElementOffsetTop = $targetElement.offset().top;
+Html5Qrcode.getCameras()
+	.then(devices => {
+		if (devices && devices.length) startCamera(devices[0].id);
+	})
+	.catch(err => {
+		console.error(err);
+	});
 
-if (!!$('.LayoutBase-emergency').text()) {
-	if ($('.LayoutBase-secondary').hasClass('isFixed')) {
-		targetElementOffsetTop += 150;
-	} else {
-		targetElementOffsetTop += 80;
-	}
-	targetElementOffsetTop -= 390;
-} else {
-	if ($('.LayoutBase-secondary').hasClass('isFixed')) {
-		targetElementOffsetTop -= 180;
-	} else {
-		targetElementOffsetTop -= 260;
-	}
+function startCamera(cameraID) {
+	const html5QrCode = new Html5Qrcode('qrreader');
+
+	html5QrCode
+		.start(
+			cameraID,
+			{
+				fps: 10, // Optional frame per seconds for qr code scanning
+				qrbox: 250, // Optional if you want bounded box UI
+			},
+			qrCodeMessage => {
+				console.log('sucess');
+			},
+			errorMessage => {
+				// parse error, ignore it.
+			}
+		)
+		.catch(err => {
+			console.error(err);
+		});
 }
-
-$('html, body').scrollTop(targetElementOffsetTop);
