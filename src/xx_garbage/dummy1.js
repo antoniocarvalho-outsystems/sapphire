9d733cb97864af5297e5f20dad81a2211ab8e802
Pablo Santos <pablo.santos@outsystems.com>
2020-11-12 18:04:18 +0000

    e-signature

--- src/xx_garbage/dummy1.js
+++ src/xx_garbage/dummy1.js
@@ -1,29 +1,28 @@
-Html5Qrcode.getCameras()
-	.then(devices => {
-		if (devices && devices.length) startCamera(devices[0].id);
-	})
-	.catch(err => {
-		console.error(err);
-	});
-
-function startCamera(cameraID) {
-	const html5QrCode = new Html5Qrcode('qrreader');
-
-	html5QrCode
-		.start(
-			cameraID,
-			{
-				fps: 10, // Optional frame per seconds for qr code scanning
-				qrbox: 250, // Optional if you want bounded box UI
-			},
-			qrCodeMessage => {
-				console.log('sucess');
-			},
-			errorMessage => {
-				// parse error, ignore it.
-			}
-		)
+SapphireWidgets.ActionsSubMenu = function() {
+	Html5Qrcode.getCameras()
+		.then(devices => {
+			if (devices && devices.length) startCamera(devices[0].id);
+		})
 		.catch(err => {
 			console.error(err);
 		});
-}
+
+	function startCamera(cameraID) {
+		const html5QrCode = new Html5Qrcode('qrreader');
+
+		html5QrCode
+			.start(
+				cameraID,
+				{ fps: 5, qrbox: 250 },
+				qrCodeMessage => {
+					console.log('sucess');
+				},
+				errorMessage => {
+					// parse error, ignore it.
+				}
+			)
+			.catch(err => {
+				console.error(err);
+			});
+	}
+};
