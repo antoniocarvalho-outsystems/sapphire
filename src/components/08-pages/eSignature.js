38d99f56358263edb21ad71f7318ab82689e6c7b
Pablo Santos <pablo.santos@outsystems.com>
2020-11-13 11:59:47 +0000

    e-signature

---
+++ src/components/08-pages/eSignature.js
@@ -0,0 +1,33 @@
+SapphireWidgets.QRCodeScanner = function() {
+	Html5Qrcode.getCameras()
+		.then(devices => {
+			if (devices && devices.length) startCamera(devices[0].id);
+		})
+		.catch(err => {
+			console.error(err);
+
+			// App doesn't have access to the camera...
+		});
+
+	function startCamera(cameraID) {
+		const html5QrCode = new Html5Qrcode('qrreader');
+		const config = { fps: 5, qrbox: 250 };
+
+		const successCallback = response => {
+			console.log(response);
+		};
+
+		const errorCallback = response => {
+			// ?
+		};
+
+		html5QrCode
+			.start(cameraID, config, successCallback, errorCallback)
+			.then(() => {
+				setTimeout(() => $('.LayoutScanCode__Splash').fadeOut(500), 500);
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
+};
