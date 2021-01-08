6f63d9366989fce1bc27e0816b4c8ed278dd9624
Pablo Santos <pablo.santos@outsystems.com>
2021-01-08 17:16:20 +0000

    feat: collapsible-side-panel

--- src/components/05-components/v3-pat/collapsible-side-panel/scripts.js
+++ src/components/05-components/v3-pat/collapsible-side-panel/scripts.js
@@ -9,13 +9,26 @@
 			this.onComponentInit();
 		}
 
-		openCloseSidePanel(toOpen) {}
+		openCloseSidePanel(toOpen) {
+			if (toOpen) {
+				const $clinicianAside = $('.ClinicianWorkArea-columns-small');
+
+				if ($clinicianAside.length) this.$content.height($clinicianAside.outerHeight());
+
+				this.$component.addClass('CollapsibleSidePanel--open');
+			} else {
+				this.$component.removeClass('CollapsibleSidePanel--open');
+			}
+		}
 
 		onComponentInit() {
 			this.$component = $(`#${this.options.widgetId}`);
 			this.$header = this.$component.find('.CollapsibleSidePanel__Header');
+			this.$content = this.$component.find('.CollapsibleSidePanel__Content');
+			this.$close = this.$component.find('.CollapsibleSidePanel__PanelHeader .icon');
 
 			this.$header.on('click', () => this.openCloseSidePanel(true));
+			this.$close.on('click', () => this.openCloseSidePanel(false));
 		}
 	}
 
