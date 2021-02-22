85131ee068d778f97820df30d46aead2f7095cad
Pablo Santos <pablo.santos@outsystems.com>
2021-02-22 12:14:45 +0000

    link hover

--- src/components/05-components/v3-pat/collapsible-side-panel/scripts.js
+++ src/components/05-components/v3-pat/collapsible-side-panel/scripts.js
@@ -58,7 +58,7 @@
 		this.counter1.text(counter);
 		this.counter2.text(counter);
 
-		if ((counter = 0)) this.counter2.addClass('ColorLightGreyBG');
+		if (+counter === 0) this.counter2.addClass('ColorLightGreyBG');
 	};
 
 	const close = widgetId => {
