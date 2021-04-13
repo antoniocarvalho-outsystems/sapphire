53872458b90356969079ea8db4439905708cdefc
Pablo Santos <pablo.santos@outsystems.com>
2021-04-13 14:19:37 +0100

    doctor activity

---
+++ src/components/08-pages/clinicianWorkArea.js
@@ -0,0 +1,25 @@
+SapphireWidgets.ClinicianWorkArea = function(containerID) {
+	$(document).ready(function() {
+		const $container = $(`${containerID}`);
+		const $toggler = $container.find('.SectionExpandable-toggler');
+
+		if ($toggler.length === 1) {
+			let $togglerState = false;
+
+			$toggler.find('[data-labelvalue]').text($toggler.find('[data-labelshow]').data('labelshow'));
+
+			$toggler.off('click').on('click', function(evt) {
+				evt.stopPropagation();
+				$togglerState = !$togglerState;
+
+				if ($togglerState) {
+					$container.find('.SectionExpandable-toggled').show();
+					$toggler.find('[data-labelvalue]').text($toggler.find('[data-labelhide]').data('labelhide'));
+				} else {
+					$container.find('.SectionExpandable-toggled').hide();
+					$toggler.find('[data-labelvalue]').text($toggler.find('[data-labelshow]').data('labelshow'));
+				}
+			});
+		}
+	});
+};
