70f73122e96f3cfac7f27ea3b164de4a2e31c0cd
Pablo Santos <pablo.santos@outsystems.com>
2021-04-19 16:51:57 +0100

    ui improvements

--- src/components/05-components/v3-pat/person-card/scripts.js
+++ src/components/05-components/v3-pat/person-card/scripts.js
@@ -1,18 +1,23 @@
 /* Component PersonCard */
 $(document).ready(function() {
 	var PersonCardEvent = function() {
-		$('.IsExpandable .PersonCard__title, .IsExpandable .PersonCard__content')
+		$('.IsExpandable .PersonCard__headerLeftInfo, .IsExpandable .PersonCard__content')
 			.off('click')
 			.on('click', function() {
 				$header = $(this).closest('.PersonCard_header');
 				$content = $header.next();
+
 				if ($content.children().length > 0) {
-					$content.slideToggle(500);
+					//$content.slideToggle(500);
+					$content.removeClass('IsExpanded');
+
 					if ($('.PersonCard.IsOpen').length > 0) {
 						$(this)
 							.closest('.PersonCard')
 							.removeClass('IsOpen');
 					} else {
+						$content.addClass('IsExpanded');
+
 						$(this)
 							.closest('.PersonCard')
 							.addClass('IsOpen');
