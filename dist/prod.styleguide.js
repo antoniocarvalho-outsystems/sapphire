1fad5fcb4c3f0068678b735daef1f664d8f20541
Pablo Santos <pablo.santos@outsystems.com>
2021-05-19 16:18:12 +0100

    New Release

--- dist/prod.styleguide.js
+++ dist/prod.styleguide.js
@@ -1,4 +1,4 @@
-/*! prod.styleguide.js || Version: 5.1.112001 || Generated: Wed May 19 2021 15:43:09 GMT+0100 (Western European Summer Time) */
+/*! prod.styleguide.js || Version: 5.1.112002 || Generated: Wed May 19 2021 16:18:00 GMT+0100 (Western European Summer Time) */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
