c5519f65dae4a7df9c1a5622a55d5cf0da1b8551
Pablo Santos <pablo.santos@outsystems.com>
2021-07-12 16:06:29 +0100

    fix tooltip

--- src/components/05-components/v3-pat/table-form/scripts.js
+++ src/components/05-components/v3-pat/table-form/scripts.js
@@ -1,10 +1,21 @@
-// /* Component TableForm */
-// (function($, window, SapphireWidgets) {
-// 	const addEmptyLine = config => {
-// 		const $list = $('.TableFormColumn:not(TableFormColumn--editMode) .TableFormColumn__Fields .ListRecords');
-// 		debugger;
-// 		$list.append('<div class="TableFormColumn__EmptyItem"></div>');
-// 	};
+/* Component TableForm */
+(function($, window, SapphireWidgets) {
+	const addEmptyLine = config => {
+		const $list = $('.TableFormColumn:not(.TableFormColumn--editMode) .TableFormColumn__Fields .ListRecords');
 
-// 	SapphireWidgets.TableForm = { addEmptyLine };
-// })(jQuery, window, SapphireWidgets);
+		$list.append('<div class="TableFormColumn__EmptyItem"></div>');
+	};
+
+	const onComponentReload = widgetId => {
+		const $table = $(`#${widgetId}`);
+		const $editing = $table.find('.TableFormColumn--editMode');
+
+		if ($editing.length) {
+			$table.addClass('TableForm--scrollDisabled');
+		} else {
+			$table.removeClass('TableForm--scrollDisabled');
+		}
+	};
+
+	SapphireWidgets.TableForm = { addEmptyLine, onComponentReload };
+})(jQuery, window, SapphireWidgets);
